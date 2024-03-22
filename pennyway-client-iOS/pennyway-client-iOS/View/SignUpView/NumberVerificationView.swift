fffbd68e675c07a8820714f96247690345b2521e
heejinnn <gml0593@gmail.com>
2024-03-22 23:07:35 +0900

    feat:예외처리 로직 ViewModel로 분리

--- pennyway-client-iOS/pennyway-client-iOS/View/SignUpView/NumberVerificationView.swift
+++ pennyway-client-iOS/pennyway-client-iOS/View/SignUpView/NumberVerificationView.swift
@@ -1,11 +1,9 @@
 import SwiftUI
 
 struct NumberVerificationView: View {
+    @StateObject var viewModel = PhoneNumberVerificationViewModel()
     @State private var verificationCode: String = ""
     @Binding var showErrorVerificationCode: Bool
-    @State var showErrorPhoneNumberFormat = false
-    @State var randomVerificationCode = ""
-    @State var phoneNumber: String = ""
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -24,7 +22,7 @@ struct NumberVerificationView: View {
             }
         }
         .onChange(of: verificationCode) { newValue in
-            if newValue == randomVerificationCode {
+            if newValue == viewModel.randomVerificationCode {
                 showErrorVerificationCode = false
             } else {
                 showErrorVerificationCode = true
@@ -44,44 +42,37 @@ struct NumberVerificationView: View {
                         RoundedRectangle(cornerRadius: 4)
                             .fill(Color("Gray01"))
                             .frame(height: 46)
-                        TextField("'-' 제외 입력", text: $phoneNumber)
+                        TextField("'-' 제외 입력", text: $viewModel.phoneNumber)
                             .padding(.leading, 13)
                             .font(.pretendard(.medium, size: 14))
                             .keyboardType(.numberPad)
                         
-                            .onChange(of: phoneNumber) { newValue in
+                            .onChange(of: viewModel.phoneNumber) { newValue in
                                 if Int(newValue) != nil {
                                     if newValue.count > 11 {
-                                        phoneNumber = String(newValue.prefix(11))
+                                        viewModel.phoneNumber = String(newValue.prefix(11))
                                     }
                                 } else {
-                                    phoneNumber = ""
+                                    viewModel.phoneNumber = ""
                                 }
                             }
                     }
                     Button(action: {
-                 
-                        if phoneNumber.prefix(3) != "010" && phoneNumber.prefix(3) != "011" {
-                            showErrorPhoneNumberFormat = true
-                        }else{
-                            randomVerificationCode = String(Int.random(in: 100000...999999))
-                            print(randomVerificationCode)
-                        }
-                        
-                                 
+                        viewModel.validatePhoneNumber()
+                        viewModel.generateRandomVerificationCode()
                     }, label: {
                         Text("인증번호 받기")
                             .font(.pretendard(.medium, size: 13))
-                            .platformTextColor(color: phoneNumber.count >= 11 ? Color("White") : Color("Gray04"))
+                            .platformTextColor(color: viewModel.phoneNumber.count >= 11 ? Color("White") : Color("Gray04"))
                     })
                     .padding(.horizontal, 13)
                     .frame(height: 46)
-                    .background(phoneNumber.count == 11 ? Color("Gray05"): Color("Gray03"))
+                    .background(viewModel.phoneNumber.count == 11 ? Color("Gray05"): Color("Gray03"))
                     .clipShape(RoundedRectangle(cornerRadius: 4))
                 }
                 .padding(.horizontal, 20)
             }
-            if showErrorPhoneNumberFormat {
+            if viewModel.showErrorPhoneNumberFormat {
                 Text("존재하지 않는 전화번호예요")
                     .padding(.horizontal, 20)
                     .font(.pretendard(.medium, size: 12))
---
+++ pennyway-client-iOS/pennyway-client-iOS/ViewModel/PhoneNumberVerificationViewModel.swift
@@ -0,0 +1,23 @@
+
+import SwiftUI
+
+class PhoneNumberVerificationViewModel: ObservableObject {
+    @Published var phoneNumber: String = ""
+    @Published var showErrorPhoneNumberFormat = false
+    @Published var randomVerificationCode = ""
+    
+    func generateRandomVerificationCode() {
+        if !showErrorPhoneNumberFormat{
+            randomVerificationCode = String(Int.random(in: 100000...999999))
+            print(randomVerificationCode)
+        }
+    }
+    
+    func validatePhoneNumber() {
+        if phoneNumber.prefix(3) != "010" && phoneNumber.prefix(3) != "011" {
+            showErrorPhoneNumberFormat = true
+        }else{
+            showErrorPhoneNumberFormat = false
+        }
+    }
+}
