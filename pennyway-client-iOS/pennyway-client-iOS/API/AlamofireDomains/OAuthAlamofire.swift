9777725b4bba0322874800f1412f47d6305b3440
heejinnn <gml0593@gmail.com>
2024-04-10 02:25:22 +0900

    feat: oauth 회원가입/연동 api 설계

--- pennyway-client-iOS/pennyway-client-iOS/API/AlamofireDomains/OAuthAlamofire.swift
+++ pennyway-client-iOS/pennyway-client-iOS/API/AlamofireDomains/OAuthAlamofire.swift
@@ -14,11 +14,11 @@ class OAuthAlamofire: TokenHandling {
         session = Session(eventMonitors: monitors)
     }
     
-    func oauthLogin(_ oauthID: String, _ idToken: String, _ provider: String, completion: @escaping (Result<Data?, Error>) -> Void) {
-        os_log("OAuthAlamofire - oauthLogin() called : %@ ,,  %@ ,, %@", log: .default, type: .info, oauthID, idToken, provider)
+    func oauthLogin(_ oauthId: String, _ idToken: String, _ provider: String, completion: @escaping (Result<Data?, Error>) -> Void) {
+        os_log("OAuthAlamofire - oauthLogin() called : %@ ,,  %@ ,, %@", log: .default, type: .info, oauthId, idToken, provider)
         
         session
-            .request(OAuthRouter.oauthLogin(oauthID: oauthID, idToken: idToken, provider: provider))
+            .request(OAuthRouter.oauthLogin(oauthId: oauthId, idToken: idToken, provider: provider))
             .response { response in
                 switch response.result {
                 case let .success(data):
@@ -49,6 +49,35 @@ class OAuthAlamofire: TokenHandling {
         
         session
             .request(OAuthRouter.oauthVerifyVerificationCode(phone: phone, code: code, provider: provider))
+            .response { response in
+                switch response.result {
+                case let .success(data):
+                    completion(.success(data))
+                case let .failure(error):
+                    completion(.failure(error))
+                }
+            }
+    }
+    
+    func linkOAuthWithNormalAccount(_ idToken: String, _ phone: String, _ provider: String, completion: @escaping (Result<Data?, Error>) -> Void) {
+        os_log("OAuthAlamofire - linkOAuthWithNormalAccount() called : %@ ,, %@ ,, %@", log: .default, type: .info, idToken, phone, provider)
+        
+        session
+            .request(OAuthRouter.linkOAuthWithNormalAccount(idToken: idToken, phone: phone, provider: provider))
+            .response { response in
+                switch response.result {
+                case let .success(data):
+                    completion(.success(data))
+                case let .failure(error):
+                    completion(.failure(error))
+                }
+            }
+    }
+    func oauthRegist(_ idToken: String, _ name: String, _ username: String, _ phone: String, _ code: String, _ provider: String, completion: @escaping (Result<Data?, Error>) -> Void) {
+        os_log("OAuthAlamofire - oauthRegist() called : %@ ,, %@ ,, %@ ,, %@ ,, %@ ,, %@", log: .default, type: .info, idToken, name , username, phone, code, provider)
+        
+        session
+            .request(OAuthRouter.oauthRegist(idToken: idToken, name: name,  username: username, phone: phone, code: code, provider: provider))
             .response { response in
                 switch response.result {
                 case let .success(data):
