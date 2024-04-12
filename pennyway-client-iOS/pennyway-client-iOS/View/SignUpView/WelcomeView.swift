cd9989dbc8882014361d297f25162dea97c6f3f8
yanni <122153297+yanni13@users.noreply.github.com>
2024-04-12 19:59:11 +0900

    fix: welcomeview에서 navigationbarbutton 삭제

--- pennyway-client-iOS/pennyway-client-iOS/View/SignUpView/WelcomeView.swift
+++ pennyway-client-iOS/pennyway-client-iOS/View/SignUpView/WelcomeView.swift
@@ -4,46 +4,37 @@ struct WelcomeView: View {
     /// @ObservedObject var formViewModel: SignUpFormViewModel
     var body: some View {
         ZStack {
-            VStack {
-                Image("icon_illust_welcome")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(maxWidth: .infinity, maxHeight: 160)
-                    .padding(.top, 71)
-                    .padding(.horizontal, 80)
-                    .padding(.bottom, 20)
-                
-                Text("\(RegistrationManager.shared.name!)님 환영합니다.")
-                    .font(.pretendard(.semibold, size: 24))
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal, 73)
-                    .padding(.bottom, 12)
-                    .onAppear {
-                        // print("\(formViewModel.name)님 환영합니다.")
-                        print("\(RegistrationManager.shared.name)님 환영합니다.")
-                    }
-                
-                Text("페니웨이와 절약을 시작해볼까요?")
-                    .font(.pretendard(.medium, size: 14))
-                    .platformTextColor(color: Color("Gray04"))
-                
-                Spacer()
-                
-                CustomBottomButton(action: {}, label: "확인", isFormValid: .constant(true))
-                    .padding(.bottom, 34)
-            }
-        }
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                HStack {
-                    NavigationBackButton()
-                        .padding(.leading, 5)
-                        .frame(width: 44, height: 44)
-                        .contentShape(Rectangle())
+            NavigationAvailable {
+                VStack {
+                    Image("icon_illust_welcome")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(maxWidth: .infinity, maxHeight: 160)
+                        .padding(.top, 71)
+                        .padding(.horizontal, 80)
+                        .padding(.bottom, 20)
                     
-                }.offset(x: -10)
+                    Text("\(RegistrationManager.shared.name!)님 환영합니다.")
+                        .font(.pretendard(.semibold, size: 24))
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 73)
+                        .padding(.bottom, 12)
+                        .onAppear {
+                            // print("\(formViewModel.name)님 환영합니다.")
+                            print("\(RegistrationManager.shared.name)님 환영합니다.")
+                        }
+                    
+                    Text("페니웨이와 절약을 시작해볼까요?")
+                        .font(.pretendard(.medium, size: 14))
+                        .platformTextColor(color: Color("Gray04"))
+                    
+                    Spacer()
+                    
+                    CustomBottomButton(action: {}, label: "확인", isFormValid: .constant(true))
+                        .padding(.bottom, 34)
+                }
             }
+            .navigationBarBackButtonHidden(true)
         }
     }
 }
