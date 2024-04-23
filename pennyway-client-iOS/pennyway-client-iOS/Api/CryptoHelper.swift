32757c9fb1655afee3f032977e85d05cc37735f7
heejinnn <gml0593@gmail.com>
2024-04-24 01:58:42 +0900

    feat: nonce crypto-helper 파일 생성

---
+++ pennyway-client-iOS/pennyway-client-iOS/Api/CryptoHelper.swift
@@ -0,0 +1,49 @@
+
+import CryptoKit
+import Foundation
+
+enum CryptoHelper {
+    static func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+
+        let charset: [Character] = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+
+                return random
+            }
+
+            for random in randoms {
+                if remainingLength == 0 {
+                    continue
+                }
+
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+
+        return result
+    }
+
+    static func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            String(format: "%02x", $0)
+        }.joined()
+
+        return hashString
+    }
+}
