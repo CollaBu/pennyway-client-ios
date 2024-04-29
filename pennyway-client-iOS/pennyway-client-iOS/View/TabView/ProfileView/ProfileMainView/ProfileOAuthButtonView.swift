c0c0ecaad3fb11324d1f3c998908988fb930cc49
heejinnn <gml0593@gmail.com>
2024-04-30 05:20:28 +0900

    feat: profile-setting-list view ui 구현

--- pennyway-client-iOS/pennyway-client-iOS/View/TabView/ProfileView/ProfileMainView/ProfileOAuthButtonView.swift
+++ pennyway-client-iOS/pennyway-client-iOS/View/TabView/ProfileView/ProfileMainView/ProfileOAuthButtonView.swift
@@ -4,7 +4,6 @@ import SwiftUI
 struct ProfileOAuthButtonView: View {
     var body: some View {
         VStack(alignment: .center) {
-            
             Spacer().frame(height: 24 * DynamicSizeFactor.factor())
             
             Text("SNS 연동 현황 확인하기")
@@ -12,20 +11,17 @@ struct ProfileOAuthButtonView: View {
                 .platformTextColor(color: Color("Gray04"))
             
             HStack(spacing: 10 * DynamicSizeFactor.factor()) {
-                Button(action: {
-                }, label: {
+                Button(action: {}, label: {
                     Image("icon_signin_kakao")
                         .frame(width: 30 * DynamicSizeFactor.factor(), height: 30 * DynamicSizeFactor.factor())
                 })
                
-                Button(action: {
-                }, label: {
+                Button(action: {}, label: {
                     Image("icon_signin_google")
                         .frame(width: 30 * DynamicSizeFactor.factor(), height: 30 * DynamicSizeFactor.factor())
                 })
                
-                Button(action: {
-                }, label: {
+                Button(action: {}, label: {
                     Image("icon_signin_apple")
                         .frame(width: 30 * DynamicSizeFactor.factor(), height: 30 * DynamicSizeFactor.factor())
                 })
--- pennyway-client-iOS/pennyway-client-iOS/View/TabView/ProfileView/ProfileMainView/ProfileSettingListView.swift
+++ pennyway-client-iOS/pennyway-client-iOS/View/TabView/ProfileView/ProfileMainView/ProfileSettingListView.swift
@@ -1,14 +1,46 @@
-
-
 import SwiftUI
 
 struct ProfileSettingListView: View {
     var body: some View {
-        VStack {
-            Text("Subview 3")
+        VStack{
+            
+            Spacer().frame(height: 32 * DynamicSizeFactor.factor())
+            
+            LazyVStack(spacing: 0) {
+                SectionView(title: "내 정보", items: ["내 정보 수정", "내가 쓴 글", "스크랩", "비밀번호 변경"])
+                SectionView(title: "앱 설정", items: ["알림 설정"])
+                SectionView(title: "이용안내", items: ["문의하기"])
+                SectionView(title: "기타", items: ["로그아웃", "회원탈퇴"])
+            }
         }
-
-        .frame(maxWidth: .infinity)
+        .frame(maxWidth: .infinity, minHeight: 511)
         .background(Color("White01"))
     }
 }
+
+struct SectionView: View {
+    let title: String
+    let items: [String]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.B2SemiboldFont())
+                .platformTextColor(color: Color("Gray04"))
+                .background(Color(UIColor.systemBackground))
+            
+            Spacer().frame(height: 14 * DynamicSizeFactor.factor())
+            
+            ForEach(items, id: \.self) { item in
+                Text(item)
+                    .font(.H4MediumFont())
+                    .platformTextColor(color: Color("Gray07"))
+                    .padding(.vertical, 7)
+            }
+            Spacer().frame(height: 14 * DynamicSizeFactor.factor())
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal, 21)
+    }
+}
+
