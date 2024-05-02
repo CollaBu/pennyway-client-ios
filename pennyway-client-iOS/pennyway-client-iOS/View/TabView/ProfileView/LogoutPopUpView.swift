e75f4c6796f6c243ffbab65416594f78eda09ec9
yanni <122153297+yanni13@users.noreply.github.com>
2024-05-03 01:15:52 +0900

    fix: findidname filename 수정

---
+++ pennyway-client-iOS/pennyway-client-iOS/View/TabView/ProfileView/LogoutPopUpView.swift
@@ -0,0 +1,13 @@
+
+
+import SwiftUI
+
+struct LogoutPopUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogoutPopUpView()
+}
