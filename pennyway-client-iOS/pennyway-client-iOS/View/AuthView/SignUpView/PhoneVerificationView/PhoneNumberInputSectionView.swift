02c65f7ca8bee8239a349d95d80a15521e5e6dda
heejinnn <gml0593@gmail.com>
2024-05-17 01:09:08 +0900

    fix: phonenumber inputsection 동적 ui + placeholder 수정

--- pennyway-client-iOS/pennyway-client-iOS/View/AuthView/SignUpView/PhoneVerificationView/PhoneNumberInputSectionView.swift
+++ pennyway-client-iOS/pennyway-client-iOS/View/AuthView/SignUpView/PhoneVerificationView/PhoneNumberInputSectionView.swift
@@ -13,14 +13,23 @@ struct PhoneNumberInputSectionView: View {
                     .font(.pretendard(.regular, size: 12))
                     .platformTextColor(color: Color("Gray04"))
                 HStack(spacing: 11) {
-                    ZStack {
+                    ZStack(alignment: .leading) {
                         RoundedRectangle(cornerRadius: 4)
                             .fill(Color("Gray01"))
-                            .frame(height: 46)
-                        TextField("'-' 제외 입력", text: $viewModel.phoneNumber)
-                            .padding(.leading, 13)
-                            .font(.pretendard(.medium, size: 14))
+                            .frame(height: 46 * DynamicSizeFactor.factor())
+
+                        if viewModel.phoneNumber.isEmpty {
+                            Text("01012345678")
+                                .platformTextColor(color: Color("Gray03"))
+                                .padding(.leading, 13 * DynamicSizeFactor.factor())
+                                .font(.H4MediumFont())
+                        }
+
+                        TextField("", text: $viewModel.phoneNumber)
+                            .padding(.leading, 13 * DynamicSizeFactor.factor())
+                            .font(.H4MediumFont())
                             .keyboardType(.numberPad)
+                            .platformTextColor(color: Color("Gray07"))
 
                             .onChange(of: viewModel.phoneNumber) { newValue in
                                 if Int(newValue) != nil {
@@ -33,6 +42,7 @@ struct PhoneNumberInputSectionView: View {
                                 viewModel.validateForm()
                             }
                     }
+
                     Button(action: {
                         if isOAuthRegistration {
                             viewModel.requestOAuthVerificationCodeApi { viewModel.judgeTimerRunning() }
@@ -41,11 +51,11 @@ struct PhoneNumberInputSectionView: View {
                         }
                     }, label: {
                         Text("인증번호 받기")
-                            .font(.pretendard(.medium, size: 13))
+                            .font(.pretendard(.medium, size: 13)) // 폰트 리스트에 없는 예외
                             .platformTextColor(color: !viewModel.isDisabledButton && viewModel.phoneNumber.count >= 11 ? Color("White01") : Color("Gray04"))
                     })
-                    .padding(.horizontal, 13)
-                    .frame(height: 46)
+                    .padding(.horizontal, 13 * DynamicSizeFactor.factor())
+                    .frame(height: 46 * DynamicSizeFactor.factor())
                     .background(!viewModel.isDisabledButton && viewModel.phoneNumber.count == 11 ? Color("Gray05") : Color("Gray03"))
                     .clipShape(RoundedRectangle(cornerRadius: 4))
                     .disabled(viewModel.isDisabledButton)
@@ -55,14 +65,14 @@ struct PhoneNumberInputSectionView: View {
             if viewModel.showErrorPhoneNumberFormat {
                 Text("올바른 전화번호 형식이 아니에요")
                     .padding(.horizontal, 20)
-                    .font(.pretendard(.medium, size: 12))
+                    .font(.B1MediumFont())
                     .platformTextColor(color: Color("Red03"))
             }
 
             if viewModel.showErrorExistingUser {
                 Text("이미 가입된 전화번호예요")
                     .padding(.horizontal, 20)
-                    .font(.pretendard(.medium, size: 12))
+                    .font(.B1MediumFont())
                     .platformTextColor(color: Color("Red03"))
             }
         }
