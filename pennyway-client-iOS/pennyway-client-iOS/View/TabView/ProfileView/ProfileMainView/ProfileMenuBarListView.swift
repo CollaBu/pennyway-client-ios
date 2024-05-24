cf4ce42051edbdeb22674a0cacf1b6f55757aa34
yanni <122153297+yanni13@users.noreply.github.com>
2024-05-24 15:01:55 +0900

    🎨 프로필 UI 햄버거바 navigation연결 (#70)
    * feat: 프로필 햄버거바 ui 연결
    
    * fix: profilemenuBarList navigationbar 수정

---
+++ pennyway-client-iOS/pennyway-client-iOS/View/TabView/ProfileView/ProfileMainView/ProfileMenuBarListView.swift
@@ -0,0 +1,36 @@
+
+import SwiftUI
+
+struct ProfileMenuBarListView: View {
+    var body: some View {
+        ScrollView {
+            VStack {
+                ProfileOAuthButtonView()
+
+                Spacer().frame(height: 9 * DynamicSizeFactor.factor())
+
+                ProfileSettingListView()
+            }
+            .background(Color("Gray01"))
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .setTabBarVisibility(isHidden: false)
+        .navigationBarColor(UIColor(named: "White01"), title: "")
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                HStack {
+                    NavigationBackButton()
+                        .padding(.leading, 5)
+                        .frame(width: 44, height: 44)
+                        .contentShape(Rectangle())
+
+                }.offset(x: -10)
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfileMenuBarListView()
+}
