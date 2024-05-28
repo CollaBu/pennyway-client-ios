96c94c62563ce83f3fb93537c22448dc22c2f5b3
heejinnn <gml0593@gmail.com>
2024-05-29 02:38:35 +0900

    feat: categorylist enum 설정

--- pennyway-client-iOS/pennyway-client-iOS/Model/ListItem/CategoryIconListItem.swift
+++ pennyway-client-iOS/pennyway-client-iOS/Model/ListItem/CategoryIconListItem.swift
@@ -6,3 +6,19 @@ struct CategoryIconListItem: Identifiable {
     let offIcon: String
     let onIcon: String
 }
+
+enum SpendingCategoryIconList: String {
+    case other = "OTHER"
+    case food = "FOOD"
+    case transportation = "TRANSPORTATION"
+    case beautyOrFashion = "BEAUTY_OR_FASHION"
+    case convenienceStore = "CONVENIENCE_STORE"
+    case education = "EDUCATION"
+    case living = "LIVING"
+    case health = "HEALTH"
+    case hobby = "HOBBY"
+    case travel = "TRAVEL"
+    case alcoholOrEntertainment = "ALCOHOL_OR_ENTERTAINMENT"
+    case membershipOrFamilyEvent = "MEMBERSHIP_OR_FAMILY_EVENT"
+}
+
