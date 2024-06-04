781cb6ac27c22632047922ef19aabf6e5a717c8b
heejinnn <gml0593@gmail.com>
2024-06-05 01:52:02 +0900

    feat: navigation bar 적용

--- pennyway-client-iOS/pennyway-client-iOS/View/TabView/SpendingManagementView/TargetAmountView/TotalTargetAmountView.swift
+++ pennyway-client-iOS/pennyway-client-iOS/View/TabView/SpendingManagementView/TargetAmountView/TotalTargetAmountView.swift
@@ -11,7 +11,44 @@ struct TotalTargetAmountView: View {
                 Spacer().frame(height: 29 * DynamicSizeFactor.factor())
             }
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color("Gray01"))
+        .navigationBarColor(UIColor(named: "Mint03"), title: "")
+        .setTabBarVisibility(isHidden: true)
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                HStack {
+                    Button(action: {
+                        NavigationUtil.popToRootView()
+                    }, label: {
+                        Image("icon_arrow_back")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 34, height: 34)
+                            .padding(5)
+                    })
+                    .padding(.leading, 5)
+                    .frame(width: 44, height: 44)
+                    .contentShape(Rectangle())
+
+                }.offset(x: -10)
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                HStack(spacing: 0) {
+                    Button(action: {}, label: {
+                        Image("icon_navigationbar_kebabmenu")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 24 * DynamicSizeFactor.factor(), height: 24 * DynamicSizeFactor.factor())
+                            .padding(5)
+                    })
+                    .padding(.trailing, 5)
+                    .frame(width: 44, height: 44)
+                }
+                .offset(x: 10)
+            }
+        }
     }
 }
 
