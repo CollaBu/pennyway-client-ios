4277d0be7e2da3e36034abaa6fbd4f9108833b54
heejinnn <gml0593@gmail.com>
2024-06-27 00:16:16 +0900

    feat: 주석 추가

--- pennyway-client-iOS/pennyway-client-iOS/ViewModel/TargetAmountViewModel/TotalTargetAmountViewModel.swift
+++ pennyway-client-iOS/pennyway-client-iOS/ViewModel/TargetAmountViewModel/TotalTargetAmountViewModel.swift
@@ -5,9 +5,9 @@ import SwiftUI
 // MARK: - TotalTargetAmountViewModel
 
 class TotalTargetAmountViewModel: ObservableObject {
-    @Published var targetAmounts: [TargetAmount] = []
-    @Published var sortTargetAmounts: [TargetAmount] = []
-    @Published var currentData: TargetAmount = TargetAmount(year: 0, month: 0, targetAmountDetail: AmountDetail(id: -1, amount: -1, isRead: false), totalSpending: 0, diffAmount: 0)
+    @Published var targetAmounts: [TargetAmount] = [] // 내림차순 데이터
+    @Published var sortTargetAmounts: [TargetAmount] = [] // 오름차순 정렬
+    @Published var currentData: TargetAmount = TargetAmount(year: 0, month: 0, targetAmountDetail: AmountDetail(id: -1, amount: -1, isRead: false), totalSpending: 0, diffAmount: 0) // 당월 데이터
 
     func getTotalTargetAmountApi(completion: @escaping (Bool) -> Void) {
         let getTotalTargetAmountRequestDto = GetTotalTargetAmountRequestDto(date: Date.getBasicformattedDate(from: Date()))
@@ -45,4 +45,3 @@ class TotalTargetAmountViewModel: ObservableObject {
         }
     }
 }
-
