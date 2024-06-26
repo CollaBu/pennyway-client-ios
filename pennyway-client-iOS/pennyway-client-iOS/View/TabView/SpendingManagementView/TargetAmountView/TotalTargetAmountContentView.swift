9484fba0af5f1b2f495c91baa875fc9eae8828d5
heejinnn <gml0593@gmail.com>
2024-06-26 19:31:20 +0900

    feat: 목표 금액 리스트 조회 api 데이터 연동

--- pennyway-client-iOS/pennyway-client-iOS/View/TabView/SpendingManagementView/TargetAmountView/TotalTargetAmountContentView.swift
+++ pennyway-client-iOS/pennyway-client-iOS/View/TabView/SpendingManagementView/TargetAmountView/TotalTargetAmountContentView.swift
@@ -21,7 +21,7 @@ struct TotalTargetAmountContentView: View {
                 
                 Spacer()
                 
-                Text("\(viewModel.currentData.diffAmount)원")
+                Text(viewModel.currentData.targetAmountDetail.amount != -1 ? "\(viewModel.currentData.diffAmount)원" : "-원")
                     .font(.B1SemiboldeFont())
                     .platformTextColor(color: determineDiffAmountColor(for: viewModel.currentData.diffAmount))
                     .padding(.trailing, 16)
@@ -98,7 +98,11 @@ struct TotalTargetAmountContentView: View {
     // Color 설정
     
     func determineDiffAmountColor(for diffAmount: Int) -> Color {
-        return diffAmount <= 0 ? Color("Red03") : Color("Gray07")
+        if viewModel.currentData.targetAmountDetail.amount != -1 {
+            return diffAmount <= 0 ? Color("Red03") : Color("Gray07")
+        } else {
+            return Color("Gray07")
+        }
     }
     
     func determineBackgroundColor(for diffAmount: Int) -> Color {
--- pennyway-client-iOS/pennyway-client-iOS/View/TabView/SpendingManagementView/TargetAmountView/TotalTargetAmountGraphView.swift
+++ pennyway-client-iOS/pennyway-client-iOS/View/TabView/SpendingManagementView/TargetAmountView/TotalTargetAmountGraphView.swift
@@ -5,21 +5,36 @@ struct TotalTargetAmountGraphView: View {
     @ObservedObject var viewModel: TotalTargetAmountViewModel
     var body: some View {
         HStack(spacing: 24 * DynamicSizeFactor.factor()) {
-            ForEach(Array(viewModel.targetAmounts.prefix(6).enumerated()), id: \.offset) { _, content in
-
-                VStack {
-                    Text("\(content.totalSpending / 10000)")
-                        .font(.B3MediumFont())
-                        .platformTextColor(color: determineColorGray04(for: content))
-                    Rectangle()
-                        .frame(maxWidth: 16 * DynamicSizeFactor.factor(), maxHeight: CGFloat(content.totalSpending / 10000) * DynamicSizeFactor.factor())
-                        .platformTextColor(color: determineColorGray03(for: content))
-                        .clipShape(RoundedCornerUtil(radius: 15, corners: [.topLeft, .topRight]))
-                    Text("\(content.month)월")
-                        .font(.B3MediumFont())
-                        .platformTextColor(color: determineColorGray06(for: content))
+            ForEach(0 ..< 6) { index in
+                if index >= 6 - viewModel.sortTargetAmounts.count {
+                    let content = viewModel.sortTargetAmounts[index - (6 - viewModel.sortTargetAmounts.count)]
+                    VStack {
+                        Text("\(content.totalSpending / 10000)")
+                            .font(.B3MediumFont())
+                            .platformTextColor(color: determineColorGray04(for: content))
+                        Rectangle()
+                            .frame(maxWidth: 16 * DynamicSizeFactor.factor(), maxHeight: CGFloat(content.totalSpending / 10000) * DynamicSizeFactor.factor())
+                            .platformTextColor(color: determineColorGray03(for: content))
+                            .clipShape(RoundedCornerUtil(radius: 15, corners: [.topLeft, .topRight]))
+                        Text("\(content.month)월")
+                            .font(.B3MediumFont())
+                            .platformTextColor(color: determineColorGray06(for: content))
+                    }
+                    .frame(maxHeight: .infinity, alignment: .bottom)
+                } else {
+                    VStack {
+                        Text("0")
+                            .font(.B3MediumFont())
+                            .platformTextColor(color: Color("Gray03"))
+                        Rectangle()
+                            .frame(maxWidth: 16 * DynamicSizeFactor.factor(), maxHeight: 0)
+                            .foregroundColor(.clear)
+                        Text("\(viewModel.currentData.month - (6 - (index + 1)))월")
+                            .font(.B3MediumFont())
+                            .platformTextColor(color: Color("Gray03")) 
+                    }
+                    .frame(maxHeight: .infinity, alignment: .bottom)
                 }
-                .frame(maxHeight: .infinity, alignment: .bottom)
             }
         }
         .frame(maxWidth: .infinity)
