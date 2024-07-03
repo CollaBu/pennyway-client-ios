c09acbba7847c381924d0f308c87d0afd9af6ef7
heejinnn <gml0593@gmail.com>
2024-07-03 13:59:58 +0900

    feat: 카테고리 관리 뷰 + 버튼 추가

--- pennyway-client-iOS/pennyway-client-iOS/View/TabView/SpendingManagementView/AddSpendingHistoryView/SpendingCategory/SpendingCategoryGridView.swift
+++ pennyway-client-iOS/pennyway-client-iOS/View/TabView/SpendingManagementView/AddSpendingHistoryView/SpendingCategory/SpendingCategoryGridView.swift
@@ -10,18 +10,20 @@ struct SpendingCategoryGridView: View {
     var body: some View {
         ZStack {
             ScrollView {
+                Spacer().frame(height: 16 * DynamicSizeFactor.factor())
+                
                 VStack(alignment: .leading, spacing: 0) {
-                    LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3), spacing: 20) {
+                    LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3), spacing: 8 * DynamicSizeFactor.factor()) {
                         ForEach(viewModel.systemCategories) { category in
-                            VStack {
+                            VStack(spacing: 0) {
                                 Image("\(category.icon.rawValue)")
                                     .resizable()
                                     .aspectRatio(contentMode: .fit)
                                     .frame(width: 52 * DynamicSizeFactor.factor(), height: 52 * DynamicSizeFactor.factor())
                                 
                                 Text(category.name)
-                                    .font(.subheadline)
-                                    .foregroundColor(Color("Gray07"))
+                                    .font(.B1MediumFont())
+                                    .platformTextColor(color: Color("Gray07"))
                             }
                             .frame(width: 88 * DynamicSizeFactor.factor(), height: 92 * DynamicSizeFactor.factor())
                             .background(Color("White01"))
@@ -34,22 +36,23 @@ struct SpendingCategoryGridView: View {
                     Spacer().frame(height: 20 * DynamicSizeFactor.factor())
                     
                     Text("내가 추가한")
-                        .font(.subheadline)
+                        .font(.B1MediumFont())
+                        .platformTextColor(color: Color("Gray07"))
                         .padding(.horizontal, 20)
                     
                     Spacer().frame(height: 12 * DynamicSizeFactor.factor())
                     
-                    LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3), spacing: 8) {
+                    LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3), spacing: 8 * DynamicSizeFactor.factor()) {
                         ForEach(viewModel.customCategories) { category in
-                            VStack {
+                            VStack(spacing: 0) {
                                 Image("\(category.icon.rawValue)")
                                     .resizable()
                                     .aspectRatio(contentMode: .fit)
                                     .frame(width: 52 * DynamicSizeFactor.factor(), height: 52 * DynamicSizeFactor.factor())
                                 
                                 Text(category.name)
-                                    .font(.subheadline)
-                                    .foregroundColor(Color("Gray07"))
+                                    .font(.B1MediumFont())
+                                    .platformTextColor(color: Color("Gray07"))
                             }
                             .frame(width: 88 * DynamicSizeFactor.factor(), height: 92 * DynamicSizeFactor.factor())
                             .background(Color("White01"))
@@ -59,13 +62,14 @@ struct SpendingCategoryGridView: View {
                     }
                     .padding(.horizontal, 20)
                     
-                    Spacer().frame(height: 21)
+                    Spacer().frame(height: 24 * DynamicSizeFactor.factor())
                 }
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color("Gray01"))
             .navigationBarColor(UIColor(named: "Gray01"), title: "카테고리")
             .navigationBarBackButtonHidden(true)
+            .edgesIgnoringSafeArea(.bottom)
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
                     HStack {
@@ -83,6 +87,21 @@ struct SpendingCategoryGridView: View {
                         .contentShape(Rectangle())
 
                     }.offset(x: -10)
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    HStack(spacing: 0) {
+                        Button(action: {
+                            
+                        }, label: {
+                            Image("icon_navigation_add_black")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 28 * DynamicSizeFactor.factor(), height: 28 * DynamicSizeFactor.factor())
+                                .padding(5 * DynamicSizeFactor.factor())
+                        })
+                        .padding(.trailing, 5 * DynamicSizeFactor.factor())
+                        .frame(width: 44, height: 44)
+                    }.offset(x: 10)
                 }
             }
         }
