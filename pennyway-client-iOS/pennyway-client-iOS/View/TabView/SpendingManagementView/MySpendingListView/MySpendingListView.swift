a2851d742c9084c682d158d1133bb32a17b7a189
최희진 <103185302+heejinnn@users.noreply.github.com>
2024-07-09 11:49:57 +0900

    🛠️🎨 카테고리 선택시 아이콘 색상 변경 (#101)
    * fix: resolve merge conflict
    
    * feat: 카테고리 아이콘 white 적용
    
    (cherry picked from commit 98f53220cacd5bd0ee824dd9ea0c8d9ffd9d5e30)
    
    * feat: mint 아이콘 반영
    
    * feat: mint 아이콘으로 변경 적용
    
    * feat: bottom-button 터치 영역 수정
    
    * feat: mint - gray 아이콘 매칭 적용
    
    * refactor: 카테고리 아이콘 관리 로직 수정

--- pennyway-client-iOS/pennyway-client-iOS/View/TabView/SpendingManagementView/MySpendingListView/MySpendingListView.swift
+++ pennyway-client-iOS/pennyway-client-iOS/View/TabView/SpendingManagementView/MySpendingListView/MySpendingListView.swift
@@ -96,7 +96,7 @@ struct MySpendingListView: View {
         .setTabBarVisibility(isHidden: true)
         .navigationBarBackButtonHidden(true)
         .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
+            ToolbarItem(placement: .topBarLeading) {
                 HStack {
                     NavigationBackButton()
                         .padding(.leading, 5)
--- pennyway-client-iOS/pennyway-client-iOS/ViewModel/SpendingViewModel/SpendingCategoryViewModel.swift
+++ pennyway-client-iOS/pennyway-client-iOS/ViewModel/SpendingViewModel/SpendingCategoryViewModel.swift
@@ -13,7 +13,6 @@ class SpendingCategoryViewModel: ObservableObject {
     
     /// 사용자 정의 카테고리 리스트
     @Published var customCategories: [SpendingCategoryData] = []
-    
     @Published var dailyDetailSpendings: [IndividualSpending] = [IndividualSpending(id: 0, amount: 10000, category: SpendingCategory(isCustom: false, id: 0, name: "식비", icon: "FOOD"), spendAt: "2024-07-04", accountName: "abc", memo: "그냥"), IndividualSpending(id: 1, amount: 10000, category: SpendingCategory(isCustom: false, id: 1, name: "식비", icon: "FOOD"), spendAt: "2024-07-04", accountName: "abc", memo: "그냥"), IndividualSpending(id: 2, amount: 10000, category: SpendingCategory(isCustom: false, id: 2, name: "식비", icon: "FOOD"), spendAt: "2024-07-04", accountName: "abc", memo: "그냥"), IndividualSpending(id: 3, amount: 30000, category: SpendingCategory(isCustom: false, id: 3, name: "식비", icon: "TRAVEL"), spendAt: "2024-07-02", accountName: "abc", memo: "그냥"), IndividualSpending(id: 4, amount: 40000, category: SpendingCategory(isCustom: false, id: 4, name: "여행", icon: "TRAVEL"), spendAt: "2024-07-02", accountName: "abc", memo: "몰라")] // 지출내역 리스트 임시 데이터
     
     func getSpendingCustomCategoryListApi(completion: @escaping (Bool) -> Void) {
@@ -34,7 +33,6 @@ class SpendingCategoryViewModel: ObservableObject {
                         let otherCategory = SpendingCategoryIconList.plus.details
                         self.customCategories = response.data.spendingCategories.compactMap { self.convertToSpendingCategoryData(from: $0) }
                         self.spendingCategories = self.systemCategories + self.customCategories + [otherCategory]
-                        
                         completion(true)
                     } catch {
                         Log.fault("Error decoding JSON: \(error)")
