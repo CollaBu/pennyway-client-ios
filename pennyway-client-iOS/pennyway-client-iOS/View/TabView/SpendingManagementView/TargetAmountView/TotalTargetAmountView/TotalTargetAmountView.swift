dea74e06892281cc1588a9af25e92ea1ee39ffad
heejinnn <gml0593@gmail.com>
2024-07-17 13:36:28 +0900

    feat: 목표 금액 삭제 ui 구현

--- pennyway-client-iOS/pennyway-client-iOS/View/TabView/SpendingManagementView/TargetAmountView/TotalTargetAmountView/TotalTargetAmountView.swift
+++ pennyway-client-iOS/pennyway-client-iOS/View/TabView/SpendingManagementView/TargetAmountView/TotalTargetAmountView/TotalTargetAmountView.swift
@@ -8,6 +8,7 @@ struct TotalTargetAmountView: View {
     @State private var selectedMenu: String? = nil // 선택한 메뉴
     @State private var listArray: [String] = ["목표금액 수정", "초기화하기"]
     @State private var navigateToEditTarget = false
+    @State private var showingDeletePopUp = false
 
     var body: some View {
         ZStack {
@@ -30,6 +31,8 @@ struct TotalTargetAmountView: View {
                             onItemSelected: { item in
                                 if item == "목표금액 수정" {
                                     navigateToEditTarget = true
+                                } else {
+                                    showingDeletePopUp = true
                                 }
                                 Log.debug("Selected item: \(item)")
                             }
@@ -37,54 +40,68 @@ struct TotalTargetAmountView: View {
                     }
                 }, alignment: .topTrailing
             )
-            NavigationLink(destination: TargetAmountSettingView(currentData: $viewModel.currentData), isActive: $navigateToEditTarget) {}
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color("Gray01"))
-        .navigationBarColor(UIColor(named: "Mint03"), title: "")
-        .edgesIgnoringSafeArea(.bottom)
-        .setTabBarVisibility(isHidden: true)
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                HStack {
-                    Button(action: {
-                        NavigationUtil.popToRootView()
-                    }, label: {
-                        Image("icon_arrow_back_white")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 34, height: 34)
-                            .padding(5)
-                    })
-                    .padding(.leading, 5)
-                    .frame(width: 44, height: 44)
-                    .contentShape(Rectangle())
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color("Gray01"))
+            .navigationBarColor(UIColor(named: "Mint03"), title: "")
+            .edgesIgnoringSafeArea(.bottom)
+            .setTabBarVisibility(isHidden: true)
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    HStack {
+                        Button(action: {
+                            NavigationUtil.popToRootView()
+                        }, label: {
+                            Image("icon_arrow_back_white")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 34, height: 34)
+                                .padding(5)
+                        })
+                        .padding(.leading, 5)
+                        .frame(width: 44, height: 44)
+                        .contentShape(Rectangle())
 
-                }.offset(x: -10)
-            }
-            ToolbarItem(placement: .topBarTrailing) {
-                HStack(spacing: 0) {
-                    Button(action: {
-                        isClickMenu.toggle()
-                        selectedMenu = nil
-                    }, label: {
-                        Image("icon_navigationbar_kebabmenu_white")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 24 * DynamicSizeFactor.factor(), height: 24 * DynamicSizeFactor.factor())
-                            .padding(5)
-                    })
-                    .padding(.trailing, 5)
-                    .frame(width: 44, height: 44)
+                    }.offset(x: -10)
                 }
-                .offset(x: 10)
+                ToolbarItem(placement: .topBarTrailing) {
+                    HStack(spacing: 0) {
+                        Button(action: {
+                            isClickMenu.toggle()
+                            selectedMenu = nil
+                        }, label: {
+                            Image("icon_navigationbar_kebabmenu_white")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 24 * DynamicSizeFactor.factor(), height: 24 * DynamicSizeFactor.factor())
+                                .padding(5)
+                        })
+                        .padding(.trailing, 5)
+                        .frame(width: 44, height: 44)
+                    }
+                    .offset(x: 10)
+                }
+            }
+
+            if showingDeletePopUp {
+                Color.black.opacity(0.3).edgesIgnoringSafeArea(.all)
+                CustomPopUpView(showingPopUp: $showingDeletePopUp,
+                                titleLabel: "목표금액을 초기화할까요?",
+                                subTitleLabel: "이번 달 목표금액이 사라져요",
+                                firstBtnAction: { self.showingDeletePopUp = false },
+                                firstBtnLabel: "취소",
+                                secondBtnAction: {},
+                                secondBtnLabel: "초기화하기",
+                                secondBtnColor: Color("Mint03")
+                )
             }
         }
         .onAppear {
-            viewModel.getTotalTargetAmountApi { _ in 
+            viewModel.getTotalTargetAmountApi { _ in
             }
         }
+
+        NavigationLink(destination: TargetAmountSettingView(currentData: $viewModel.currentData), isActive: $navigateToEditTarget) {}
     }
 }
 
