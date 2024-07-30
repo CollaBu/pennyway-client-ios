de555bd9cf1e9e4c633b8b72c7a19c7689eaa5f1
yanni <122153297+yanni13@users.noreply.github.com>
2024-07-31 01:10:49 +0900

    fix: 변경사항 저장

--- pennyway-client-iOS/pennyway-client-iOS/View/TabView/MainTabView.swift
+++ pennyway-client-iOS/pennyway-client-iOS/View/TabView/MainTabView.swift
@@ -16,23 +16,23 @@ struct MainTabView: View {
                         Text("지출관리")
                     }
                     .tag(0)
-                
+
                 PreparedFeedView()
                     .tabItem {
                         selection == 1 ? Image("icon_tapbar_feed_on") : Image("icon_tapbar_feed_off")
                         Text("피드")
                     }
                     .tag(1)
-                
+
                 PreparedChatView()
                     .tabItem {
                         selection == 2 ? Image("icon_tapbar_chatting_on") : Image("icon_tapbar_chatting_off")
                         Text("채팅")
                     }
                     .tag(2)
-                
+
                 ProfileMainView()
-                
+
                     .tabItem {
                         selection == 3 ? Image("icon_tabbar_profile_on") : Image("icon_tabbar_profile_off")
                         Text("프로필")
