2741c590bad8c2549099fc03738e1e61cbad46bd
JaeSeo <qud1251@gmail.com>
2024-07-30 15:28:15 +0900

    fix: 분석 상수 all 메서드 로직 수정

--- pennyway-client-iOS/pennyway-client-iOS/Analytics/AnalyticsEvent.swift
+++ pennyway-client-iOS/pennyway-client-iOS/Analytics/AnalyticsEvent.swift
@@ -58,30 +58,47 @@ extension AnalyticsEvent {
     /**
      *  서비스 내 화면 정보를 상수로써 정의한다.
      */
-    enum Screen {
-        static let loginView = ScreenViewEvent(name: "로그인 화면", className: "LoginView")
+    enum Screen: String, CaseIterable {
+        case loginView
+        
+        var event: ScreenViewEvent {
+            switch self {
+            case .loginView:
+                return ScreenViewEvent(name: "로그인 화면", className: "LoginView")
+            }
+        }
     }
     
     /**
      *  서비스 내 이벤트 정보를 상수로써 정의한다.
      */
-    enum Trigger {
-        static let login = EventLog(name: "user_login", parameters: nil)
+    enum Trigger: String, CaseIterable {
+        case login
+        case buttonTap
         
-        static let buttonTap = EventLog(name: "button_tapped", parameters: nil)
+        var event: EventLog {
+            switch self {
+            case .login:
+                return EventLog(name: "user_login", parameters: nil)
+            case .buttonTap:
+                return EventLog(name: "button_tapped", parameters: nil)
+            }
+        }
     }
     
     /**
      *  이 속성은 오직 편의용 메서드로 사용한다.
-     *  만약, analytics serivce에서 모든 이벤트를 구독할 때, 이 메서드를 사용하면 정의된 모든 이벤트를 반환한다.
+     *  만약, analytics serivce에서 모든 페이지를 구독할 때, 이 메서드를 사용하면 정의된 모든 페이지 정보를 반환한다.
      */
-    static var all: [AnalyticsEvent] = {
-        var events = [AnalyticsEvent]()
-        
-        AnalyticsEvent.all.forEach { AnalyticsEvent in
-            events.append(AnalyticsEvent)
-        }
-        
-        return events
+    static var allScreenEvent: [ScreenViewEvent] = {
+        return Screen.allCases.map { $0.event }
+    }()
+    
+    /**
+     *  이 속성은 오직 편의용 메서드로 사용한다.
+     *  만약, analytics serivce에서 모든 이벤트를 구독할 때, 이 메서드를 사용하면 정의된 모든 이벤트 정보를 반환한다.
+     */
+    static var allTriggerEvent: [EventLog] = {
+        return Trigger.allCases.map { $0.event }
     }()
 }
