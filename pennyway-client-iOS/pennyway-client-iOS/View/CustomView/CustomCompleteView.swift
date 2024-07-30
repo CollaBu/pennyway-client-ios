691ebaeba29db1ee8c83226d89adf5d395eb56c2
heejinnn <gml0593@gmail.com>
2024-07-31 02:48:55 +0900

    feat: 완료 뷰 커스텀으로 분리

---
+++ pennyway-client-iOS/pennyway-client-iOS/View/CustomView/CustomCompleteView.swift
@@ -0,0 +1,32 @@
+
+import SwiftUI
+
+struct CustomCompleteView: View {
+    let title: String
+    let subTitle: String
+
+    var body: some View {
+        VStack {
+                Spacer().frame(height: 171 * DynamicSizeFactor.factor())
+                    
+                Image("icon_illust_completion")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 68 * DynamicSizeFactor.factor(), height: 68 * DynamicSizeFactor.factor())
+                    
+                Spacer().frame(height: 17 * DynamicSizeFactor.factor())
+                    
+                Text(title)
+                    .font(.H3SemiboldFont())
+                    .multilineTextAlignment(.center)
+                    
+                Spacer().frame(height: 16 * DynamicSizeFactor.factor())
+                    
+                Text(subTitle)
+                    .font(.H4MediumFont())
+                    .platformTextColor(color: Color("Gray04"))
+                    
+                Spacer()
+        }
+    }
+}
