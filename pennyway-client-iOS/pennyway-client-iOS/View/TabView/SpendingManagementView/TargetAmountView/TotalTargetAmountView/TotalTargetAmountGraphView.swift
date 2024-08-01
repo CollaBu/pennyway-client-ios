53acc35edaeee439c765eda52ebafbde08b67314
heejinnn <gml0593@gmail.com>
2024-08-01 21:37:29 +0900

    feat: 그래프 비율 조정하는 로직 수정

--- pennyway-client-iOS/pennyway-client-iOS/View/TabView/SpendingManagementView/TargetAmountView/TotalTargetAmountView/TotalTargetAmountGraphView.swift
+++ pennyway-client-iOS/pennyway-client-iOS/View/TabView/SpendingManagementView/TargetAmountView/TotalTargetAmountView/TotalTargetAmountGraphView.swift
@@ -4,21 +4,21 @@ import SwiftUI
 struct TotalTargetAmountGraphView: View {
     @ObservedObject var viewModel: TotalTargetAmountViewModel
     var body: some View {
-        let maxHeight = 140 * DynamicSizeFactor.factor()//최대 높이
+        let maxHeight = 120 * DynamicSizeFactor.factor() // 최대 높이
         let maxSpending = max(viewModel.maxTotalSpending, 100_000) // 최소값을 100000으로 설정
 
         HStack(spacing: 24 * DynamicSizeFactor.factor()) {
             ForEach(0 ..< 6) { index in
                 if index >= 6 - viewModel.sortTargetAmounts.count {
                     let content = viewModel.sortTargetAmounts[index - (6 - viewModel.sortTargetAmounts.count)]
-                    let adjustedHeight = (maxSpending > 0) ? CGFloat(content.totalSpending) / CGFloat(maxSpending) * maxHeight : 0//그래프 높이 조정
+                    let adjustedHeight = (maxSpending > 0) ? CGFloat(content.totalSpending) / CGFloat(maxSpending) * maxHeight : 0 // 그래프 높이 조정
 
                     VStack {
                         Text("\(content.totalSpending / 10000)")
                             .font(.B3MediumFont())
                             .platformTextColor(color: determineColorGray04(for: content))
                         Rectangle()
-                            .frame(maxWidth: 16 * DynamicSizeFactor.factor(), maxHeight: adjustedHeight)
+                            .frame(width: 16 * DynamicSizeFactor.factor(), height: adjustedHeight)
                             .platformTextColor(color: determineColorGray03(for: content))
                             .clipShape(RoundedCornerUtil(radius: 15, corners: [.topLeft, .topRight]))
 
