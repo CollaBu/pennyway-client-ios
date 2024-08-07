20a355124046f57e6f72c2083470ca3b8700e45f
yanni <122153297+yanni13@users.noreply.github.com>
2024-08-08 00:40:48 +0900

    feat: 알림 조회 api 설정

---
+++ pennyway-client-iOS/pennyway-client-iOS/Api/Domain/UserDomain/Dto/Response/UserAccount/NotificationContentData.swift
@@ -0,0 +1,12 @@
+
+struct NotificationContentData: Codable {
+    let id: Int
+    let isRead: Bool
+    let title: String
+    let content: String   
+    let type: String
+    let from: String
+    let fromId: Int
+    let toId: Int
+    let createdAt: String 
+}
