07f37370dd31b7233e98144e6e1e59877fa8cb84
heejinnn <gml0593@gmail.com>
2024-08-15 01:18:32 +0900

    feat: fcm 토큰 전송 api 요청 위치 수정

--- pennyway-client-iOS/pennyway-client-iOS/App/AppDelegate.swift
+++ pennyway-client-iOS/pennyway-client-iOS/App/AppDelegate.swift
@@ -63,31 +63,6 @@ extension AppDelegate: MessagingDelegate {
         if let fcmToken = fcmToken {
             AppDelegate.currentFCMToken = fcmToken // fcm 토큰 저장
             Log.info("fcmToken: \(fcmToken)")
-            registDeviceTokenApi(fcmToken: fcmToken)
-        }
-    }
-
-    private func registDeviceTokenApi(fcmToken: String) {
-        let fcmTokenDto = FcmTokenDto(token: fcmToken)
-
-        UserAccountAlamofire.shared.registDeviceToken(fcmTokenDto) { result in
-            switch result {
-            case let .success(data):
-                if let responseData = data {
-                    do {
-                        let response = try JSONDecoder().decode(ErrorResponseDto.self, from: responseData)
-                        Log.debug(response)
-                    } catch {
-                        Log.fault("Error parsing response JSON: \(error)")
-                    }
-                }
-            case let .failure(error):
-                if let statusSpecificError = error as? StatusSpecificError {
-                    Log.info("StatusSpecificError occurred: \(statusSpecificError)")
-                } else {
-                    Log.error("Network request failed: \(error)")
-                }
-            }
         }
     }
 }
