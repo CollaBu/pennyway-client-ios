3f3895d5d6fe821a5e9b0530a78bd1b7f706fd8a
JaeSeo <qud1251@gmail.com>
2024-08-14 13:59:09 +0900

    feat: 소비내역 카테고리 이벤트 상수 정의(클래스명 제외)

---
+++ pennyway-client-iOS/pennyway-client-iOS/Analytics/Event/SpendingCategoryAnalyticsEvents.swift
@@ -0,0 +1,82 @@
+//
+//  SpendingCategoryAnalyticsEvents.swift
+//  pennyway-client-iOS
+//
+//  Created by 양재서 on 8/14/24.
+//
+
+enum SpendingCategoryEvents: AnalyticsEvent {
+    case categorySelectView // 카테고리 선택 뷰
+    case categoryAddView // 카테고리 추가 뷰
+    case iconSelectView // 아이콘 선택 뷰
+    case categoryListView // 카테고리 리스트 뷰
+    case categoryDetailView // 카테고리 상세 뷰
+    case categoryUpdateView // 카테고리 수정 뷰
+    case categoryDeleteView // 카테고리 삭제 뷰
+    case categoryMigrateView // 카테고리 이전 뷰 (소비 내역 옮김)
+    
+    case migrateSpendingList // 소비 내역 이전하기
+    case deleteCategoryList // 소비 내역 삭제하기
+}
+
+enum SpendingCategoryScreen {
+    case categorySelectView
+    case categoryAddView
+    case iconSelectView
+    case categoryListView
+    case categoryDetailView
+    case categoryUpdateView
+    case categoryDeleteView
+    case categoryMigrateView
+    
+    var screenId: String {
+        switch self {
+        case .categorySelectView: return "category_select_view"
+        case .categoryAddView: return "category_add_view"
+        case .iconSelectView: return "icon_select_view"
+        case .categoryListView: return "category_list_view"
+        case .categoryDetailView: return "category_detail_view"
+        case .categoryUpdateView: return "category_update_view"
+        case .categoryDeleteView: return "category_delete_view"
+        case .categoryMigrateView: return "category_migrate_view"
+        }
+    }
+    
+    var screenName: String {
+        switch self {
+        case .categorySelectView: return "카테고리 선택 화면"
+        case .categoryAddView: return "카테고리 추가 화면"
+        case .iconSelectView: return "아이콘 선택 화면"
+        case .categoryListView: return "카테고리 리스트 화면"
+        case .categoryDetailView: return "카테고리 상세 화면"
+        case .categoryUpdateView: return "카테고리 수정 화면"
+        case .categoryDeleteView: return "카테고리 삭제 화면"
+        case .categoryMigrateView: return "카테고리 내 소비내역 옮기기 화면"
+        }
+    }
+    
+    var screenClass: String {
+        switch self {
+        case .categorySelectView: return ""
+        case .categoryAddView: return ""
+        case .iconSelectView: return ""
+        case .categoryListView: return ""
+        case .categoryDetailView: return ""
+        case .categoryUpdateView: return ""
+        case .categoryDeleteView: return ""
+        case .categoryMigrateView: return ""
+        }
+    }
+}
+
+enum SpendingCategoryCustomEvent {
+    case migrateSpendingList
+    case deleteCategoryList
+    
+    var eventName: String {
+        switch self {
+        case .migrateSpendingList: return "migrate_spending_list"
+        case .deleteCategoryList: return "delete_category_list"
+        }
+    }
+}
