e0db57a4aeb5a8683e47c1bdfe450cf09ae128a5
최희진 <103185302+heejinnn@users.noreply.github.com>
2024-08-15 23:11:21 +0900

    📍 AT 존재 여부에 따른 API 요청 + 사용자 알림 허용 로직  (#180)
    * feat: fcm 토큰 전송 api 요청 위치 수정
    
    * feat: access token 저장된 경우에만 요청 가능하도록 수정
    
    * feat: 앱 처음 실행시 알림 허용 로직 수정

--- pennyway-client-iOS/pennyway-client-iOS/ViewModel/AppViewModel.swift
+++ pennyway-client-iOS/pennyway-client-iOS/ViewModel/AppViewModel.swift
@@ -12,6 +12,16 @@ class AppViewModel: ObservableObject {
         checkLoginStateApi()
     }
 
+    func logout() {
+        isLoggedIn = false
+        checkLoginState = false
+    }
+
+    func login() {
+        registDeviceTokenApi()
+        isLoggedIn = true
+    }
+    
     func checkLoginStateApi() {
         UserAuthAlamofire.shared.checkLoginState { [weak self] result in
             switch result {
@@ -22,9 +32,10 @@ class AppViewModel: ObservableObject {
                         Log.debug(response)
                         self?.checkLoginState = true
                         self?.isLoggedIn = true
-
+                        
+                        self?.registDeviceTokenApi()
                         Log.debug("accessToken: \(KeychainHelper.loadAccessToken())")
-
+                        
                     } catch {
                         Log.fault("Error parsing response JSON: \(error)")
                         self?.checkLoginState = false
@@ -36,18 +47,37 @@ class AppViewModel: ObservableObject {
                 } else {
                     Log.error("Network request failed: \(error)")
                 }
-
+                
                 self?.checkLoginState = false
             }
         }
     }
-
-    func logout() {
-        isLoggedIn = false
-        checkLoginState = false
-    }
-
-    func login() {
-        isLoggedIn = true
+    
+    func registDeviceTokenApi() {
+        if let fcmToken = AppDelegate.currentFCMToken {
+            let fcmTokenDto = FcmTokenDto(token: fcmToken)
+                    
+            UserAccountAlamofire.shared.registDeviceToken(fcmTokenDto) { result in
+                switch result {
+                case let .success(data):
+                    if let responseData = data {
+                        do {
+                            let response = try JSONDecoder().decode(ErrorResponseDto.self, from: responseData)
+                            Log.debug(response)
+                        } catch {
+                            Log.fault("Error parsing response JSON: \(error)")
+                        }
+                    }
+                case let .failure(error):
+                    if let statusSpecificError = error as? StatusSpecificError {
+                        Log.info("StatusSpecificError occurred: \(statusSpecificError)")
+                    } else {
+                        Log.error("Network request failed: \(error)")
+                    }
+                }
+            }
+        } else {
+            Log.fault("fcm Token 존재 x")
+        }
     }
 }
