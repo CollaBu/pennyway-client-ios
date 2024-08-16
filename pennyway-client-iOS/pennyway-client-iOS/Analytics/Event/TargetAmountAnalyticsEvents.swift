36f786860a3a9a6eeaf46987910b6a107403da47
JaeSeo <qud1251@gmail.com>
2024-08-16 20:48:17 +0900

    feat: 목표 금액 설정 완료 뷰 이벤트 추가

--- pennyway-client-iOS/pennyway-client-iOS/Analytics/Event/TargetAmountAnalyticsEvents.swift
+++ pennyway-client-iOS/pennyway-client-iOS/Analytics/Event/TargetAmountAnalyticsEvents.swift
@@ -13,13 +13,14 @@ enum TargetAmountEvents: AnalyticsEvent {
     case targetAmountHistoryView // 지난 사용 금액 뷰
     case targetAmountInitView // 초기 목표 금액 설정 뷰
     case targetAmountUpdateView // 목표 금액 수정 뷰
+    case targetAmountSetCompleteView // 목표 금액 설정 완료 뷰
     
     case maintainRecentTargetAmount // 최근 목표 금액 유지
     case cancelRecentTotalAmount // 최근 목표 금액 유지 취소
     
     var eventName: AnalyticsConstants.EventName {
         switch self {
-        case .targetAmountView, .targetAmountHistoryView, .targetAmountInitView, .targetAmountUpdateView:
+        case .targetAmountView, .targetAmountHistoryView, .targetAmountInitView, .targetAmountUpdateView, .targetAmountSetCompleteView:
             return AnalyticsConstants.EventName.screenView
         case .maintainRecentTargetAmount, .cancelRecentTotalAmount:
             return AnalyticsConstants.EventName.btnTapped
@@ -28,7 +29,7 @@ enum TargetAmountEvents: AnalyticsEvent {
     
     var eventType: AnalyticsConstants.EventType {
         switch self {
-        case .targetAmountView, .targetAmountHistoryView, .targetAmountInitView, .targetAmountUpdateView:
+        case .targetAmountView, .targetAmountHistoryView, .targetAmountInitView, .targetAmountUpdateView, .targetAmountSetCompleteView:
             return AnalyticsConstants.EventType.screenView
         case .maintainRecentTargetAmount, .cancelRecentTotalAmount:
             return AnalyticsConstants.EventType.userAction
@@ -61,6 +62,12 @@ enum TargetAmountEvents: AnalyticsEvent {
                 .screenName: TargetAmountScreen.targetAmountUpdateView.screenName,
                 .screenClass: TargetAmountScreen.targetAmountUpdateView.screenClass
             ]
+        case .targetAmountSetCompleteView:
+            return [
+                .screenId: TargetAmountScreen.targetAmountSetCompleteView.screenId,
+                .screenName: TargetAmountScreen.targetAmountSetCompleteView.screenName,
+                .screenClass: TargetAmountScreen.targetAmountSetCompleteView.screenClass
+            ]
         case .maintainRecentTargetAmount:
             return [
                 .eventName: TargetAmountCustomEvent.maintainRecentTargetAmount.eventName
@@ -80,6 +87,7 @@ enum TargetAmountScreen {
     case targetAmountHistoryView
     case targetAmountInitView
     case targetAmountUpdateView
+    case targetAmountSetCompleteView
     
     var screenId: String {
         switch self {
@@ -87,6 +95,7 @@ enum TargetAmountScreen {
         case .targetAmountHistoryView: return "total_amount_history_view"
         case .targetAmountInitView: return "total_amount_init_view"
         case .targetAmountUpdateView: return "total_amount_update_view"
+        case .targetAmountSetCompleteView: return "total_amount_set_complete_view"
         }
     }
     
@@ -96,15 +105,17 @@ enum TargetAmountScreen {
         case .targetAmountHistoryView: return "지난 사용 금액 화면"
         case .targetAmountInitView: return "초기 목표 금액 설정 화면"
         case .targetAmountUpdateView: return "목표 금액 수정 화면"
+        case .targetAmountSetCompleteView: return "목표 금액 설정 완료 화면"
         }
     }
     
     var screenClass: String {
         switch self {
-        case .targetAmountView: return ""
-        case .targetAmountHistoryView: return ""
-        case .targetAmountInitView: return ""
-        case .targetAmountUpdateView: return ""
+        case .targetAmountView: return "TotalTargetAmountView"
+        case .targetAmountHistoryView: return "PastSpendingListView"
+        case .targetAmountInitView: return "TargetAmountSettingView"
+        case .targetAmountUpdateView: return "TargetAmountSettingView"
+        case .targetAmountSetCompleteView: return "TargetAmountSetCompleteView"
         }
     }
 }
