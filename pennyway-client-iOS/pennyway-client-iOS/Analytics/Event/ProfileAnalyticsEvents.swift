84ce47eb982c49c5aff72ed5aabd21d4b4e05fa9
JaeSeo <qud1251@gmail.com>
2024-08-21 17:41:23 +0900

    feat: firebaseAnalyticsService Profile event 등록

--- pennyway-client-iOS/pennyway-client-iOS/Analytics/Event/ProfileAnalyticsEvents.swift
+++ pennyway-client-iOS/pennyway-client-iOS/Analytics/Event/ProfileAnalyticsEvents.swift
@@ -5,6 +5,8 @@
 //  Created by 양재서 on 8/21/24.
 //
 
+// MARK: - ProfileEvents
+
 enum ProfileEvents: AnalyticsEvent {
     case profileTapView
     case profileHamburgerMenuTap
@@ -37,76 +39,78 @@ enum ProfileEvents: AnalyticsEvent {
         }
     }
     
-    var parameters: [AnalyticsConstants.Parameter : Any]? {
+    var parameters: [AnalyticsConstants.Parameter: Any]? {
         switch self {
         case .profileTapView: [
-            .screenId: ProfileScreen.profileTapView.screenId,
-            .screenName: ProfileScreen.profileTapView.screenName,
-            .screenClass: ProfileScreen.profileTapView.screenClass
-        ]
+                .screenId: ProfileScreen.profileTapView.screenId,
+                .screenName: ProfileScreen.profileTapView.screenName,
+                .screenClass: ProfileScreen.profileTapView.screenClass
+            ]
         case .profileHamburgerMenuTap: [
-            .screenId: ProfileScreen.profileHamburgerMenuTap.screenId,
-            .screenName: ProfileScreen.profileHamburgerMenuTap.screenName,
-            .screenClass: ProfileScreen.profileHamburgerMenuTap.screenClass
-        ]
+                .screenId: ProfileScreen.profileHamburgerMenuTap.screenId,
+                .screenName: ProfileScreen.profileHamburgerMenuTap.screenName,
+                .screenClass: ProfileScreen.profileHamburgerMenuTap.screenClass
+            ]
         case .profileEditView: [
-            .screenId: ProfileScreen.profileEditView.screenId,
-            .screenName: ProfileScreen.profileEditView.screenName,
-            .screenClass: ProfileScreen.profileEditView.screenClass
-        ]
+                .screenId: ProfileScreen.profileEditView.screenId,
+                .screenName: ProfileScreen.profileEditView.screenName,
+                .screenClass: ProfileScreen.profileEditView.screenClass
+            ]
         case .nameEditView: [
-            .screenId: ProfileScreen.nameEditView.screenId,
-            .screenName: ProfileScreen.nameEditView.screenName,
-            .screenClass: ProfileScreen.nameEditView.screenClass
-        ]
+                .screenId: ProfileScreen.nameEditView.screenId,
+                .screenName: ProfileScreen.nameEditView.screenName,
+                .screenClass: ProfileScreen.nameEditView.screenClass
+            ]
         case .usernameEditView: [
-            .screenId: ProfileScreen.usernameEditView.screenId,
-            .screenName: ProfileScreen.usernameEditView.screenName,
-            .screenClass: ProfileScreen.usernameEditView.screenClass
-        ]
+                .screenId: ProfileScreen.usernameEditView.screenId,
+                .screenName: ProfileScreen.usernameEditView.screenName,
+                .screenClass: ProfileScreen.usernameEditView.screenClass
+            ]
         case .phoneEditView: [
-            .screenId: ProfileScreen.phoneEditView.screenId,
-            .screenName: ProfileScreen.phoneEditView.screenName,
-            .screenClass: ProfileScreen.phoneEditView.screenClass
-        ]
+                .screenId: ProfileScreen.phoneEditView.screenId,
+                .screenName: ProfileScreen.phoneEditView.screenName,
+                .screenClass: ProfileScreen.phoneEditView.screenClass
+            ]
         case .currentPasswordCheckView: [
-            .screenId: ProfileScreen.currentPasswordCheckView.screenId,
-            .screenName: ProfileScreen.currentPasswordCheckView.screenName,
-            .screenClass: ProfileScreen.currentPasswordCheckView.screenClass
-        ]
+                .screenId: ProfileScreen.currentPasswordCheckView.screenId,
+                .screenName: ProfileScreen.currentPasswordCheckView.screenName,
+                .screenClass: ProfileScreen.currentPasswordCheckView.screenClass
+            ]
         case .passwordEditView: [
-            .screenId: ProfileScreen.passwordEditView.screenId,
-            .screenName: ProfileScreen.passwordEditView.screenName,
-            .screenClass: ProfileScreen.passwordEditView.screenClass
-        ]
+                .screenId: ProfileScreen.passwordEditView.screenId,
+                .screenName: ProfileScreen.passwordEditView.screenName,
+                .screenClass: ProfileScreen.passwordEditView.screenClass
+            ]
         case .passwordEditCompleteView: [
-            .screenId: ProfileScreen.passwordEditCompleteView.screenId,
-            .screenName: ProfileScreen.passwordEditCompleteView.screenName,
-            .screenClass: ProfileScreen.passwordEditCompleteView.screenClass
-        ]
+                .screenId: ProfileScreen.passwordEditCompleteView.screenId,
+                .screenName: ProfileScreen.passwordEditCompleteView.screenName,
+                .screenClass: ProfileScreen.passwordEditCompleteView.screenClass
+            ]
         case .profileImageEditPopUp: [
-            .screenId: ProfileScreen.profileImageEditPopUp.screenId,
-            .screenName: ProfileScreen.profileImageEditPopUp.screenName,
-            .screenClass: ProfileScreen.profileImageEditPopUp.screenClass
-        ]
+                .screenId: ProfileScreen.profileImageEditPopUp.screenId,
+                .screenName: ProfileScreen.profileImageEditPopUp.screenName,
+                .screenClass: ProfileScreen.profileImageEditPopUp.screenClass
+            ]
         case .notificationEditView: [
-            .screenId: ProfileScreen.notificationEditView.screenId,
-            .screenName: ProfileScreen.notificationEditView.screenName,
-            .screenClass: ProfileScreen.notificationEditView.screenClass
-        ]
+                .screenId: ProfileScreen.notificationEditView.screenId,
+                .screenName: ProfileScreen.notificationEditView.screenName,
+                .screenClass: ProfileScreen.notificationEditView.screenClass
+            ]
         case .accountDeletePopUp: [
-            .screenId: ProfileScreen.accountDeletePopUp.screenId,
-            .screenName: ProfileScreen.accountDeletePopUp.screenName,
-            .screenClass: ProfileScreen.accountDeletePopUp.screenClass
-        ]
+                .screenId: ProfileScreen.accountDeletePopUp.screenId,
+                .screenName: ProfileScreen.accountDeletePopUp.screenName,
+                .screenClass: ProfileScreen.accountDeletePopUp.screenClass
+            ]
         case .accountDeleteSuccessPopUp: [
-            .screenId: ProfileScreen.accountDeleteSuccessPopUp.screenId,
-            .screenName: ProfileScreen.accountDeleteSuccessPopUp.screenName,
-            .screenClass: ProfileScreen.accountDeleteSuccessPopUp.screenClass
-        ]
+                .screenId: ProfileScreen.accountDeleteSuccessPopUp.screenId,
+                .screenName: ProfileScreen.accountDeleteSuccessPopUp.screenName,
+                .screenClass: ProfileScreen.accountDeleteSuccessPopUp.screenClass
+            ]
         }
     }
 }
+
+// MARK: - ProfileScreen
 
 enum ProfileScreen {
     case profileTapView
@@ -128,55 +132,55 @@ enum ProfileScreen {
     
     var screenId: String {
         switch self {
-            case .profileTapView: return "profile_tap_view"
-            case .profileHamburgerMenuTap: return "profile_hamburger_menu_tap"
-            case .profileEditView: return "profile_edit_view"
-            case .nameEditView: return "name_edit_view"
-            case .usernameEditView: return "username_edit_view"
-            case .phoneEditView: return "phone_edit_view"
-            case .currentPasswordCheckView: return "current_password_check_view"
-            case .passwordEditView: return "password_edit_view"
-            case .passwordEditCompleteView: return "password_edit_complete_view"
-            case .profileImageEditPopUp: return "profile_image_edit_popup"
-            case .notificationEditView: return "notification_edit_view"
-            case .accountDeletePopUp: return "account_delete_popup"
-            case .accountDeleteSuccessPopUp: return "account_delete_success_popup"
+        case .profileTapView: return "profile_tap_view"
+        case .profileHamburgerMenuTap: return "profile_hamburger_menu_tap"
+        case .profileEditView: return "profile_edit_view"
+        case .nameEditView: return "name_edit_view"
+        case .usernameEditView: return "username_edit_view"
+        case .phoneEditView: return "phone_edit_view"
+        case .currentPasswordCheckView: return "current_password_check_view"
+        case .passwordEditView: return "password_edit_view"
+        case .passwordEditCompleteView: return "password_edit_complete_view"
+        case .profileImageEditPopUp: return "profile_image_edit_popup"
+        case .notificationEditView: return "notification_edit_view"
+        case .accountDeletePopUp: return "account_delete_popup"
+        case .accountDeleteSuccessPopUp: return "account_delete_success_popup"
         }
     }
  
     var screenName: String {
         switch self {
-            case .profileTapView: return "프로필 탭 바 화면"
-            case .profileHamburgerMenuTap: return "프로필 햄버거 메뉴 화면"
-            case .profileEditView: return "내 정보 수정 화면"
-            case .nameEditView: return "이름 수정 화면"
-            case .usernameEditView: return "아이디 수정 화면"
-            case .phoneEditView: return "전화번호 수정 화면"
-            case .currentPasswordCheckView: return "현재 비밀번호 확인 화면"
-            case .passwordEditView: return "비밀번호 수정 화면"
-            case .passwordEditCompleteView: return "비밀번호 수정 완료 화면"
-            case .profileImageEditPopUp: return "프로필 이미지 수정 팝업"
-            case .notificationEditView: return "알림 설정 화면"
-            case .accountDeletePopUp: return "계정 삭제 팝업"
-            case .accountDeleteSuccessPopUp: return "계정 삭제 성공 팝업"
+        case .profileTapView: return "프로필 탭 바 화면"
+        case .profileHamburgerMenuTap: return "프로필 햄버거 메뉴 화면"
+        case .profileEditView: return "내 정보 수정 화면"
+        case .nameEditView: return "이름 수정 화면"
+        case .usernameEditView: return "아이디 수정 화면"
+        case .phoneEditView: return "전화번호 수정 화면"
+        case .currentPasswordCheckView: return "현재 비밀번호 확인 화면"
+        case .passwordEditView: return "비밀번호 수정 화면"
+        case .passwordEditCompleteView: return "비밀번호 수정 완료 화면"
+        case .profileImageEditPopUp: return "프로필 이미지 수정 팝업"
+        case .notificationEditView: return "알림 설정 화면"
+        case .accountDeletePopUp: return "계정 삭제 팝업"
+        case .accountDeleteSuccessPopUp: return "계정 삭제 성공 팝업"
         }
     }
     
     var screenClass: String {
         switch self {
-            case .profileTapView: return "ProfileMainView"
-            case .profileHamburgerMenuTap: return "ProfileMenuBarListView"
-            case .profileEditView: return "EditProfileListView"
-            case .nameEditView: return "EditUsernameView"
-            case .usernameEditView: return "EditIdView"
-            case .phoneEditView: return "EditPhoneNumberView"
-            case .currentPasswordCheckView: return "ProfileModifyPwView"
-            case .passwordEditView: return "ResetPwView"
-            case .passwordEditCompleteView: return "CompleteChangePwView"
-            case .profileImageEditPopUp: return "EditProfilePopView"
-            case .notificationEditView: return "SettingAlarmView"
-            case .accountDeletePopUp: return "ProfileMenuBarListView"
-            case .accountDeleteSuccessPopUp: return "CompleteDeleteUserView"
+        case .profileTapView: return "ProfileMainView"
+        case .profileHamburgerMenuTap: return "ProfileMenuBarListView"
+        case .profileEditView: return "EditProfileListView"
+        case .nameEditView: return "EditUsernameView"
+        case .usernameEditView: return "EditIdView"
+        case .phoneEditView: return "EditPhoneNumberView"
+        case .currentPasswordCheckView: return "ProfileModifyPwView"
+        case .passwordEditView: return "ResetPwView"
+        case .passwordEditCompleteView: return "CompleteChangePwView"
+        case .profileImageEditPopUp: return "EditProfilePopView"
+        case .notificationEditView: return "SettingAlarmView"
+        case .accountDeletePopUp: return "ProfileMenuBarListView"
+        case .accountDeleteSuccessPopUp: return "CompleteDeleteUserView"
         }
     }
 }
