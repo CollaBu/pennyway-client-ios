8202d8cb03ac99443d7e89f18a716001691f126a
heejinnn <gml0593@gmail.com>
2024-08-24 03:41:15 +0900

    feat: 빈화면 터치시 textfield 포커스 해제처리

--- pennyway-client-iOS/pennyway-client-iOS/View/CustomView/CustomInputView.swift
+++ pennyway-client-iOS/pennyway-client-iOS/View/CustomView/CustomInputView.swift
@@ -1,19 +1,23 @@
 import SwiftUI
+import UIKit
+
+// MARK: - CustomInputView
 
 struct CustomInputView: View {
-    @Binding var inputText: String
-    @State var titleText: String?
-    @State var placeholder: String?
-    var onCommit: (() -> Void)?
-    var isSecureText: Bool
-    var isCustom: Bool?
-    var showDeleteButton: Bool = false
-    var deleteAction: (() -> Void)?
+    @Binding var inputText: String // 텍스트 필드에 입력된 내용을 바인딩
+    @State var titleText: String? // 타이틀 텍스트 (옵션)
+    @State var placeholder: String? // 플레이스홀더 텍스트 (옵션)
+    var onCommit: (() -> Void)? // 입력 완료 시 실행할 함수 (옵션)
+    var isSecureText: Bool // 텍스트 필드가 비밀번호 필드인지 여부
+    var isCustom: Bool? // 커스텀 텍스트 여부 (옵션)
+    var showDeleteButton: Bool = false // 삭제 버튼 표시 여부 (기본값: false)
+    var deleteAction: (() -> Void)? // 삭제 버튼 클릭 시 실행할 함수 (옵션)
+    var keyboardType: UIKeyboardType = .default // 키보드 타입 (기본값: .default)
 
     let baseAttribute: BaseAttribute = BaseAttribute(font: .B1MediumFont(), color: Color("Gray07"))
     let stringAttribute: StringAttribute = StringAttribute(text: "*", font: .B1MediumFont(), color: Color("Mint03"))
 
-    @State private var isDeleteButtonVisible: Bool = false
+    @State private var isDeleteButtonVisible: Bool = false // 삭제 버튼의 가시성 관리
 
     var body: some View {
         VStack(alignment: .leading, spacing: 13 * DynamicSizeFactor.factor()) {
@@ -42,34 +46,23 @@ struct CustomInputView: View {
                             .padding(.leading, 13 * DynamicSizeFactor.factor())
                             .font(.H4MediumFont())
                     }
-                    if isSecureText {
-                        SecureField("", text: $inputText, onCommit: {
+                    CustomTextFieldModifier(
+                        text: $inputText,
+                        isSecureText: isSecureText,
+                        onCommit: {
                             onCommit?()
                             isDeleteButtonVisible.toggle()
-                        })
-                        .autocapitalization(.none)
-                        .disableAutocorrection(true)
-                        .padding(.leading, 12 * DynamicSizeFactor.factor())
-                        .padding(.vertical, 16 * DynamicSizeFactor.factor())
-                        .font(.H4MediumFont())
-                        .onChange(of: inputText) { newValue in
-                            isDeleteButtonVisible = !newValue.isEmpty
-                        }
-
-                    } else {
-                        TextField("", text: $inputText, onCommit: {
-                            onCommit?()
-                            isDeleteButtonVisible.toggle()
-                        })
-                        .autocapitalization(.none)
-                        .disableAutocorrection(true)
-                        .padding(.leading, 12 * DynamicSizeFactor.factor())
-                        .padding(.trailing, 35 * DynamicSizeFactor.factor())
-                        .padding(.vertical, 16 * DynamicSizeFactor.factor())
-                        .font(.H4MediumFont())
-                        .onChange(of: inputText) { newValue in
-                            isDeleteButtonVisible = !newValue.isEmpty
-                        }
+                        },
+                        keyboardType: keyboardType // CustomInputView의 keyboardType 전달
+                    )
+//                    .autocapitalization(.none)
+//                    .disableAutocorrection(true)
+                    .padding(.leading, 12 * DynamicSizeFactor.factor())
+                    .padding(.trailing, 35 * DynamicSizeFactor.factor())
+                    .frame(height: 46 * DynamicSizeFactor.factor())
+                    .font(.H4MediumFont())
+                    .onChange(of: inputText) { newValue in
+                        isDeleteButtonVisible = !newValue.isEmpty
                     }
 
                     if showDeleteButton {
@@ -86,5 +79,60 @@ struct CustomInputView: View {
             }
             .padding(.horizontal, 20)
         }
+    }
+}
+
+// MARK: - CustomTextFieldModifier
+
+// CustomTextFieldModifier: UIViewRepresentable을 사용하여 SwiftUI에서 UITextField 사용
+struct CustomTextFieldModifier: UIViewRepresentable {
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var parent: CustomTextFieldModifier
+
+        init(parent: CustomTextFieldModifier) {
+            self.parent = parent
+        }
+
+        func textFieldDidEndEditing(_: UITextField) {
+            if !parent.text.isEmpty {
+                parent.onCommit?()
+            }
+        }
+
+        func textFieldDidChangeSelection(_ textField: UITextField) {
+            parent.text = textField.text ?? ""
+        }
+
+        /// Return 버튼을 눌렀을 때 호출되는 메서드
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            parent.onCommit?()
+            textField.resignFirstResponder() // 포커스를 해제하여 키보드를 닫음
+
+            return true
+        }
+    }
+
+    @Binding var text: String
+    var isSecureText: Bool
+    var onCommit: (() -> Void)?
+    var keyboardType: UIKeyboardType // 키보드 타입을 받아서 사용
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+
+    func makeUIView(context: Context) -> UITextField {
+        let textField = UITextField()
+        textField.isSecureTextEntry = isSecureText
+        textField.delegate = context.coordinator
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        textField.borderStyle = .none
+        textField.keyboardType = keyboardType // 전달받은 키보드 타입 설정
+        return textField
+    }
+
+    func updateUIView(_ uiView: UITextField, context _: Context) {
+        uiView.text = text
     }
 }
