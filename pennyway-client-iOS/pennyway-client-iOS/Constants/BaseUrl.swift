db0d045f37470631a260cb300339cf5ec615f204
yanni <122153297+yanni13@users.noreply.github.com>
2024-08-24 21:05:11 +0900

    fix: 변경사항 저장

--- pennyway-client-iOS/pennyway-client-iOS/Constants/BaseUrl.swift
+++ pennyway-client-iOS/pennyway-client-iOS/Constants/BaseUrl.swift
@@ -5,11 +5,10 @@ import Foundation
 // MARK: - API
 
 enum API {
-    static let BASE_URL: String = "https://777c-39-116-11-157.ngrok-free.app/"
-//    static let BASE_URL: String = {
-//        guard let baseURL = Bundle.main.object(forInfoDictionaryKey: "BaseURL") as? String else {
-//            fatalError("BaseURL not found in Info.plist")
-//        }
-//        return baseURL.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: "\"", with: "")
-//    }()
+    static let BASE_URL: String = {
+        guard let baseURL = Bundle.main.object(forInfoDictionaryKey: "BaseURL") as? String else {
+            fatalError("BaseURL not found in Info.plist")
+        }
+        return baseURL.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: "\"", with: "")
+    }()
 }
