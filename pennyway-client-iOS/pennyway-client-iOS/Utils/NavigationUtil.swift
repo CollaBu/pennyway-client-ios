4e7ec82ad2a37295e9c2b0d310f2407aa116e751
heejinnn <gml0593@gmail.com>
2024-08-30 00:16:57 +0900

    feat: 네비게이션 util 로직 수정

--- pennyway-client-iOS/pennyway-client-iOS/Utils/NavigationUtil.swift
+++ pennyway-client-iOS/pennyway-client-iOS/Utils/NavigationUtil.swift
@@ -11,8 +11,13 @@ enum NavigationUtil {
             .first?.windows
             .filter { $0.isKeyWindow }.first
 
-        findNavigationController(viewController: keyWindow?.rootViewController)?
+        // 루트 뷰 컨트롤러의 이름 출력
+        if let rootViewController = keyWindow?.rootViewController {
+            Log.debug("Root ViewController name: \(String(describing: type(of: rootViewController)))")
+        }
 
+        // 루트 뷰 컨트롤러에서 NavigationController를 찾아 루트로 이동
+        findNavigationController(viewController: keyWindow?.rootViewController)?
             .popToRootViewController(animated: true)
     }
 
@@ -22,53 +27,17 @@ enum NavigationUtil {
         }
 
         if let navigationController = viewController as? UINavigationController {
-            Log.debug(navigationController)
             return navigationController
         }
 
         for childViewController in viewController.children {
-            Log.debug(childViewController)
-            return findNavigationController(viewController: childViewController)
+            if let navigationController = findNavigationController(viewController: childViewController) {
+                return navigationController
+            }
         }
 
         return nil
     }
-
-//    static func popToRootView() {
-//        let keyWindow = UIApplication.shared.connectedScenes
-//
-//            .filter { $0.activationState == .foregroundActive }
-//            .compactMap { $0 as? UIWindowScene }
-//            .first?.windows
-//            .filter { $0.isKeyWindow }.first
-//
-//        // 루트 뷰 컨트롤러의 이름 출력
-//        if let rootViewController = keyWindow?.rootViewController {
-//            Log.debug("Root ViewController name: \(String(describing: type(of: rootViewController)))")
-//        }
-//
-//        // 루트 뷰 컨트롤러에서 NavigationController를 찾아 루트로 이동
-//        findNavigationController(viewController: keyWindow?.rootViewController)?
-//            .popToRootViewController(animated: true)
-//    }
-//
-//    static func findNavigationController(viewController: UIViewController?) -> UINavigationController? {
-//        guard let viewController = viewController else {
-//            return nil
-//        }
-//
-//        if let navigationController = viewController as? UINavigationController {
-//            return navigationController
-//        }
-//
-//        for childViewController in viewController.children {
-//            if let navigationController = findNavigationController(viewController: childViewController) {
-//                return navigationController
-//            }
-//        }
-//
-//        return nil
-//    }
 
     static func popToView(at index: Int) {
         let keyWindow = UIApplication.shared.connectedScenes
@@ -83,7 +52,7 @@ enum NavigationUtil {
         }
 
         let viewControllersCount = navigationController.viewControllers.count
-
+        
         Log.debug(viewControllersCount)
 
         guard index < viewControllersCount else {
