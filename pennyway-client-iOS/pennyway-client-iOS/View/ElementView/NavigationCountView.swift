7f97fc9473dc17881f68fd90a63c255e718ce552
최희진 <103185302+heejinnn@users.noreply.github.com>
2024-09-08 23:12:52 +0900

    feat: count view 아이콘 크기 조절 (#216)

--- pennyway-client-iOS/pennyway-client-iOS/View/ElementView/NavigationCountView.swift
+++ pennyway-client-iOS/pennyway-client-iOS/View/ElementView/NavigationCountView.swift
@@ -9,21 +9,21 @@ struct NavigationCountView: View {
         HStack(spacing: 8 * DynamicSizeFactor.factor()) {
             LazyHGrid(rows: [GridItem(.flexible())]) {
                 Text("1")
-                    .padding(6)
+                    .frame(width: 18 * DynamicSizeFactor.factor(), height: 18 * DynamicSizeFactor.factor())
                     .background(selectedText ?? 0 >= 1 ? Color("Gray06") : Color("Gray03"))
                     .platformTextColor(color: selectedText ?? 0 >= 1 ? Color("White01") : Color("Gray04"))
                     .clipShape(Circle())
                     .font(.B2MediumFont())
 
                 Text("2")
-                    .padding(6)
+                    .frame(width: 18 * DynamicSizeFactor.factor(), height: 18 * DynamicSizeFactor.factor())
                     .background(selectedText ?? 0 >= 2 ? Color("Gray06") : Color("Gray03"))
                     .platformTextColor(color: selectedText ?? 0 >= 2 ? Color("White01") : Color("Gray04"))
                     .clipShape(Circle())
                     .font(.B2MediumFont())
 
                 Text("3")
-                    .padding(6)
+                    .frame(width: 18 * DynamicSizeFactor.factor(), height: 18 * DynamicSizeFactor.factor())
                     .background(selectedText ?? 0 >= 3 ? Color("Gray06") : Color("Gray03"))
                     .platformTextColor(color: selectedText ?? 0 >= 3 ? Color("White01") : Color("Gray04"))
                     .clipShape(Circle())
@@ -32,6 +32,6 @@ struct NavigationCountView: View {
             Spacer()
         }
         .padding(.horizontal, 20)
-        .frame(height: 18)
+        .frame(maxHeight: 18 * DynamicSizeFactor.factor())
     }
 }
