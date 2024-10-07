f0e9cf6281fb7b4407d8339c47842f1ac50dd4e0
heejinnn <gml0593@gmail.com>
2024-10-08 02:55:32 +0900

    feat: chat-bottom bar 구현

---
+++ pennyway-client-iOS/pennyway-client-iOS/Clean/Presentation/Component/ChatBottomBar.swift
@@ -0,0 +1,73 @@
+//
+//  ChatBottomBar.swift
+//  pennyway-client-iOS
+//
+//  Created by 최희진 on 10/8/24.
+//
+
+import SwiftUI
+
+struct ChatBottomBar: View {
+    @State private var message: String = "dd"
+    @State private var showFeature: Bool = false
+
+    var body: some View {
+        VStack {
+            HStack {
+                Button(action: {
+                    showFeature.toggle()
+                }, label: {
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .frame(width: 20 * DynamicSizeFactor.factor(), height: 20 * DynamicSizeFactor.factor())
+                        .padding(.leading, 6 * DynamicSizeFactor.factor())
+                }).buttonStyle(PlainButtonStyle())
+
+                if message.isEmpty {
+                    Text("오늘은 어떤 소비를 했나요?")
+                        .platformTextColor(color: Color("Gray03"))
+                        .font(.B2MediumFont())
+                }
+
+                TextField("", text: $message)
+
+                if !message.isEmpty {
+                    Button(action: {
+                        Log.debug("Message sent: \(message)")
+                        message = ""
+                    }) {
+                        Image(systemName: "arrow.up.circle.fill")
+                            .resizable()
+                            .frame(width: 20 * DynamicSizeFactor.factor(), height: 20 * DynamicSizeFactor.factor())
+                            .padding(.trailing, 6 * DynamicSizeFactor.factor())
+                    }
+                    .transition(.opacity)
+                    .animation(.easeInOut, value: message.isEmpty)
+                }
+            }
+            .frame(height: 29 * DynamicSizeFactor.factor())
+            .background(Color("Gray02"))
+            .cornerRadius(28)
+            .padding(.horizontal, 16)
+
+            if showFeature {
+                Spacer().frame(height: 22 * DynamicSizeFactor.factor())
+
+                VStack {
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .frame(width: 37 * DynamicSizeFactor.factor(), height: 37 * DynamicSizeFactor.factor())
+
+                    Text("사진")
+                        .platformTextColor(color: Color("Gray07"))
+                        .font(.B2MediumFont())
+                }
+            }
+
+        }.background(Color("White01"))
+    }
+}
+
+#Preview {
+    ChatBottomBar()
+}
