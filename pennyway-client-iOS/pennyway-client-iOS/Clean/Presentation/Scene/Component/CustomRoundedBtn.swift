674f7ca98828acb45781e2bb0271082685309fef
yanni <122153297+yanni13@users.noreply.github.com>
2024-10-10 21:58:11 +0900

    feat: 초기세팅 설정

--- pennyway-client-iOS/pennyway-client-iOS/Clean/Presentation/Scene/Component/CustomRoundedBtn.swift
+++ pennyway-client-iOS/pennyway-client-iOS/Clean/Presentation/Scene/Component/CustomRoundedBtn.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 struct CustomRoundedBtn: View {
-    var title: String               // 버튼에 표시될 텍스트
-    var fontColor: Color            // 텍스트 색상 (폰트 색상)
-    var backgroundColor: Color      // 버튼의 배경 색상
-    var action: () -> Void          // 버튼이 눌렸을 때 실행될 동작
+    var title: String // 버튼에 표시될 텍스트
+    var fontColor: Color // 텍스트 색상 (폰트 색상)
+    var backgroundColor: Color // 버튼의 배경 색상
+    var action: () -> Void // 버튼이 눌렸을 때 실행될 동작
 
     var body: some View {
         Button(action: {
