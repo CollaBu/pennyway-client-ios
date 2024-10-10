846f0f3c39fa0588924ae642e30e51fd890d20de
yanni <122153297+yanni13@users.noreply.github.com>
2024-10-11 00:05:24 +0900

    feat: resolve merge conflict

---
+++ pennyway-client-iOS/pennyway-client-iOS/Clean/Presentation/Scene/MakeChatRoom/Component/InputContent.swift
@@ -0,0 +1,24 @@
+
+import SwiftUI
+
+// MARK: - RoomTitleInput
+
+struct RoomTitleInput: View {
+    @Binding var roomTitle: String
+    var title: String
+    var placeholder: String
+
+    let baseAttribute: BaseAttribute
+    let stringAttribute: StringAttribute
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            title.toAttributesText(base: baseAttribute, stringAttribute)
+                .font(.B1MediumFont())
+                .platformTextColor(color: Color("Gray07"))
+                .padding(.horizontal, 20)
+
+            CustomInputView(inputText: $roomTitle, placeholder: "채팅방 이름을 입력해주세요", isSecureText: false)
+        }
+    }
+}
