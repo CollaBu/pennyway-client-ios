38373fa441ba33a196014e90fb7adb04091fa069
heejinnn <gml0593@gmail.com>
2024-10-12 19:12:49 +0900

    feat: 팝업창 구현

--- pennyway-client-iOS/pennyway-client-iOS/Clean/Presentation/Scene/ChatSetting/View/ChatUserInfoView.swift
+++ pennyway-client-iOS/pennyway-client-iOS/Clean/Presentation/Scene/ChatSetting/View/ChatUserInfoView.swift
@@ -11,60 +11,53 @@ import SwiftUI
 
 struct ChatUserInfoView: View {
     @Environment(\.presentationMode) var presentationMode
+    @State private var showTransferPopUp: Bool = false // 방장 넘기기 팝업 상태
+    @State private var showKickOutPopUp: Bool = false // 내보내기 팝업 상태
 
     var body: some View {
-        VStack {
-            Spacer()
+        ZStack {
+            UserInfoContent
 
-            VStack{
-                VStack(spacing: 14 * DynamicSizeFactor.factor()) {
-                    Rectangle()
-                        .platformTextColor(color: .clear)
-                        .frame(width: 90 * DynamicSizeFactor.factor(), height: 90 * DynamicSizeFactor.factor())
-                        .background(
-                            Image("icon_illust_maintain_goal")
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: 90 * DynamicSizeFactor.factor(), height: 90 * DynamicSizeFactor.factor())
-                                .clipped()
-                        )
-                        .cornerRadius(17)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 17)
-                                .inset(by: -1.5 * DynamicSizeFactor.factor())
-                                .stroke(.white, lineWidth: 3)
-                        )
-
-                    Text("격정하는 바다오리")
-                        .font(.H3SemiboldFont())
-                        .platformTextColor(color: .gray07)
-                }
-                .offset(y: -40 * DynamicSizeFactor.factor())
-
-                HStack(spacing: 14 * DynamicSizeFactor.factor()) {
-                    ActionButton(title: "방장 넘기기", icon: "person.fill", textColor: .mint03, backgroundColor: .mint01, action: {})
-                    ActionButton(title: "내보내기", icon: "hand.raised.fill", textColor: .red03, backgroundColor: .red01, action: {})
-                }
-                
-                Spacer().frame(height: 35 * DynamicSizeFactor.factor())
+            // 방장 넘기기 팝업
+            if showTransferPopUp {
+                CustomPopUpView(showingPopUp: $showTransferPopUp,
+                                titleLabel: "방장 권한을 넘길까요?",
+                                subTitleLabel: "권한을 넘기면 다시 취소할 수 없어요",
+                                firstBtnAction: { self.showTransferPopUp = false },
+                                firstBtnLabel: "취소",
+                                secondBtnAction: {
+                                    self.showTransferPopUp = false
+                                },
+                                secondBtnLabel: "넘길래요",
+                                secondBtnColor: Color(.mint03)
+                )
             }
-            .frame(height: 173 * DynamicSizeFactor.factor())
-            .frame(maxWidth: .infinity)
-            .background(Color(.white01))
+
+            // 내보내기 팝업
+            if showKickOutPopUp {
+                CustomPopUpView(showingPopUp: $showKickOutPopUp,
+                                titleLabel: "내보내기",
+                                subTitleLabel: "\(mockMembers[1].username)님을 내보낼까요?",
+                                firstBtnAction: { self.showKickOutPopUp = false },
+                                firstBtnLabel: "취소",
+                                secondBtnAction: {
+                                    self.showKickOutPopUp = false
+                                },
+                                secondBtnLabel: "내보내기",
+                                secondBtnColor: Color(.red03)
+                )
+            }
         }
         .setTabBarVisibility(isHidden: true)
         .navigationBarBackButtonHidden(true)
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .edgesIgnoringSafeArea(.bottom)
         .background(
-//            Image("image_chat_background")
-//            .frame(maxWidth: .infinity, maxHeight: .infinity)
-//            .border(Color.black)
-            Color.red.opacity(0.1)//변경 필요
+            Color.red.opacity(0.1) // 변경 필요
         )
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
                 HStack {
-                    Button(action: {  
+                    Button(action: {
                         self.presentationMode.wrappedValue.dismiss()
                     }, label: {
                         Image("icon_close")
@@ -79,28 +72,74 @@ struct ChatUserInfoView: View {
             }
         }
     }
-}
 
-// MARK: - ActionButton
+    private var UserInfoContent: some View {
+        VStack {
+            Spacer()
 
-struct ActionButton: View {
-    let title: String
-    let icon: String
-    let textColor: Color
-    let backgroundColor: Color
-    let action: () -> Void
+            VStack(spacing: 0) {
+                VStack(spacing: 14) {
+                    Rectangle()
+                        .platformTextColor(color: .clear)
+                        .frame(width: 90 * DynamicSizeFactor.factor(), height: 90 * DynamicSizeFactor.factor())
+                        .background(
+                            Image("icon_illust_maintain_goal")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 90 * DynamicSizeFactor.factor(), height: 90 * DynamicSizeFactor.factor())
+                                .clipped()
+                        )
+                        .cornerRadius(17)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 17)
+                                .inset(by: -1.5 * DynamicSizeFactor.factor())
+                                .stroke(.white01, lineWidth: 3)
+                        )
 
-    var body: some View {
-        Button(action: action) {
-            HStack(spacing: 7) {
-                Image(systemName: icon)
-                Text(title)
+                    Text("걱정하는 바다오리")
+                        .font(.H3SemiboldFont())
+                        .platformTextColor(color: .gray07)
+                }
+                .offset(y: -35 * DynamicSizeFactor.factor())
+
+                HStack(spacing: 14 * DynamicSizeFactor.factor()) {
+                    ActionButton(title: "방장 넘기기", icon: "person.fill", textColor: .mint03, backgroundColor: .mint01, action: {
+                        showTransferPopUp = true
+                    })
+                    ActionButton(title: "내보내기", icon: "hand.raised.fill", textColor: .red03, backgroundColor: .red01, action: {
+                        showKickOutPopUp = true
+                    })
+                }
+
+                Spacer().frame(height: 50 * DynamicSizeFactor.factor())
             }
-            .frame(width: 130 * DynamicSizeFactor.factor())
-            .padding(.vertical, 14)
-            .background(backgroundColor)
-            .platformTextColor(color: textColor)
-            .cornerRadius(6)
+            .frame(height: 180 * DynamicSizeFactor.factor())
+            .frame(maxWidth: .infinity)
+            .background(Color(.white01))
+        }
+    }
+
+    // MARK: - ActionButton (Private)
+
+    private struct ActionButton: View {
+        let title: String
+        let icon: String
+        let textColor: Color
+        let backgroundColor: Color
+        let action: () -> Void
+
+        var body: some View {
+            Button(action: action) {
+                HStack(spacing: 7) {
+                    Image(systemName: icon)
+                    Text(title)
+                }
+                .frame(width: 130 * DynamicSizeFactor.factor())
+                .padding(.vertical, 14 * DynamicSizeFactor.factor())
+                .background(backgroundColor)
+                .platformTextColor(color: textColor)
+                .cornerRadius(6)
+            }
         }
     }
 }
