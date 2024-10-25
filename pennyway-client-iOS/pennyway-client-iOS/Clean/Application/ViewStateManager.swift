aa786b981f33e0db0d2325b994a5808b088525ad
heejinnn <gml0593@gmail.com>
2024-10-26 00:47:15 +0900

    feat: case 네이밍 변경

--- pennyway-client-iOS/pennyway-client-iOS/Clean/Application/ViewStateManager.swift
+++ pennyway-client-iOS/pennyway-client-iOS/Clean/Application/ViewStateManager.swift
@@ -10,8 +10,8 @@ import SwiftUI
 // MARK: - CurrentViewType
 
 enum CurrentViewType {
-    case activeApp // MainChatView와 ChatView를 제외한 모든 뷰
-    case activeChatRoomList // MainChatView일 경우(추천 채팅 제외)
+    case activeNonChat // MainChatView와 ChatView를 제외한 모든 뷰
+    case activeChatRoomCell // MainChatView일 경우(추천 채팅 제외)
     case activeChatRoom // ChatView일 경우
     case inactive
     case background
@@ -21,7 +21,7 @@ enum CurrentViewType {
 
 class ViewStateManager: ObservableObject {
     /// 현재 활성화된 뷰 타입을 저장하는 변수 (기본값: activeApp)
-    @Published var currentViewType: CurrentViewType = .activeApp
+    @Published var currentViewType: CurrentViewType = .activeNonChat
     /// 현재 표시되고 있는 뷰를 저장하는 변수
     @Published var currentView: AnyView?
 
@@ -32,18 +32,18 @@ class ViewStateManager: ObservableObject {
         currentView = AnyView(view)
 
         if view is MainChatView {
-            if selectedTab == 1 {
-                currentViewType = .activeChatRoomList
+            if selectedTab == 1 {//내 채팅인 경우
+                currentViewType = .activeChatRoomCell
                 Log.info("[ViewStateManager] View state: activeChatRoomList")
-            } else {
-                currentViewType = .activeApp
+            } else {//추천 채팅인 경우
+                currentViewType = .activeNonChat
                 Log.info("[ViewStateManager] View state: activeApp")
             }
         } else if view is ChatView {
             currentViewType = .activeChatRoom
             Log.info("[ViewStateManager] View state: activeChatRoom")
         } else {
-            currentViewType = .activeApp
+            currentViewType = .activeNonChat
             Log.info("[ViewStateManager] View state: activeApp")
         }
     }
@@ -53,7 +53,7 @@ class ViewStateManager: ObservableObject {
     func setScenePhase(_ phase: ScenePhase) {
         switch phase {
         case .active:
-            currentViewType = .activeApp
+            currentViewType = .activeNonChat
             Log.info("[ViewStateManager - setScenePhase] View state: activeApp")
         case .inactive:
             currentViewType = .inactive
@@ -62,7 +62,7 @@ class ViewStateManager: ObservableObject {
             currentViewType = .background
             Log.info("[ViewStateManager - setScenePhase] View state: background")
         @unknown default:
-            currentViewType = .activeApp
+            currentViewType = .activeNonChat
         }
     }
 }
