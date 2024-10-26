6741e24ac9aecbc2929c39d431ff56a52c6af6b9
heejinnn <gml0593@gmail.com>
2024-10-26 16:28:28 +0900

    feat: error 구독 처리

--- pennyway-client-iOS/pennyway-client-iOS/Clean/Data/Repository/Chat/DefaultChatStompRepository.swift
+++ pennyway-client-iOS/pennyway-client-iOS/Clean/Data/Repository/Chat/DefaultChatStompRepository.swift
@@ -13,6 +13,7 @@ import StompClientLib
 class DefaultChatStompRepository: NSObject, ChatStompRepository {
     private var stompClient: StompClientLib
     private var chatServerUrl: String?
+    private var connectCompletion: ((Result<Void, Error>) -> Void)?
     
     init(stompClient: StompClientLib) {
         self.stompClient = stompClient
@@ -24,7 +25,8 @@ class DefaultChatStompRepository: NSObject, ChatStompRepository {
             switch result {
             case let .success(url):
                 self?.chatServerUrl = url
-                self?.connectToSocket(completion: completion)
+                self?.connectCompletion = completion
+                self?.connectToSocket()
             case let .failure(error):
                 completion(.failure(error))
             }
@@ -41,11 +43,18 @@ class DefaultChatStompRepository: NSObject, ChatStompRepository {
     
     /// 특정 목적지로 Stomp 구독을 설정하는 메서드
     func subscribeToDestination(_: String) {}
+
+    /// 에러 처리위해 Stomp 구독을 설정하는 메서드
+    private func subscribeToErrors() {
+        let errorReceiptId = "error-receipt-\(Date().timeIntervalSince1970)"
+        stompClient.subscribeWithHeader(destination: "/user/queue/errors", withHeader: ["receipt": errorReceiptId])
+    }
     
     /// 실제로 소켓 연결을 수행하는 메서드
-    private func connectToSocket(completion: @escaping (Result<Void, Error>) -> Void) {
+    private func connectToSocket() {
         guard let url = chatServerUrl else {
-            completion(.failure(NSError(domain: "ChatStompRepository", code: 0, userInfo: [NSLocalizedDescriptionKey: "Chat server URL is not set"])))
+            connectCompletion?(.failure(NSError(domain: "ChatStompRepository", code: 0, userInfo: [NSLocalizedDescriptionKey: "Chat server URL is not set"])))
+            connectCompletion = nil
             return
         }
         
@@ -57,7 +66,6 @@ class DefaultChatStompRepository: NSObject, ChatStompRepository {
         let request = NSURLRequest(url: URL(string: url)!)
         
         stompClient.openSocketWithURLRequest(request: request, delegate: self, connectionHeaders: headers)
-        completion(.success(()))
     }
     
     /// 채팅 서버 URL을 가져오는 메서드
@@ -86,10 +94,15 @@ class DefaultChatStompRepository: NSObject, ChatStompRepository {
 extension DefaultChatStompRepository: StompClientLibDelegate {
     func stompClientDidConnect(client _: StompClientLib!) {
         Log.debug("Socket connected")
+        subscribeToErrors()//소켓 연결 성공할 때마다 error 구독
+        connectCompletion?(.success(()))
+        connectCompletion = nil
     }
     
     func stompClientDidDisconnect(client _: StompClientLib!) {
         Log.debug("Socket disconnected")
+        connectCompletion?(.failure(NSError(domain: "ChatStompRepository", code: 2, userInfo: [NSLocalizedDescriptionKey: "Socket disconnected"])))
+        connectCompletion = nil
     }
     
     func stompClient(client _: StompClientLib!, didReceiveMessageWithJSONBody _: AnyObject?, akaStringBody _: String?, withHeader _: [String: String]?, withDestination _: String) {}
@@ -100,6 +113,8 @@ extension DefaultChatStompRepository: StompClientLibDelegate {
     
     func serverDidSendError(client _: StompClientLib!, withErrorMessage description: String, detailedErrorMessage _: String?) {
         Log.error("Error: \(description)")
+        connectCompletion?(.failure(NSError(domain: "ChatStompRepository", code: 1, userInfo: [NSLocalizedDescriptionKey: description])))
+        connectCompletion = nil
     }
     
     func serverDidSendPing() {
--- pennyway-client-iOS/pennyway-client-iOS/Clean/Presentation/Scene/MainChat/View/ChatCellView.swift
+++ pennyway-client-iOS/pennyway-client-iOS/Clean/Presentation/Scene/MainChat/View/ChatCellView.swift
@@ -189,7 +189,6 @@ struct ChatCellView: View {
             }
         }
         .contentShape(Rectangle()) 
-
     }
 }
 
