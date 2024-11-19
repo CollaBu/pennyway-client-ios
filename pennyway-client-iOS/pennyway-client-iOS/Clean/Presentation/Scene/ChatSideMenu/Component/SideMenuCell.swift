020e21616318333d464cea8ae07111d0cef4a3ac
yanni <122153297+yanni13@users.noreply.github.com>
2024-11-19 23:33:24 +0900

    feat: 토글 사이즈 enum으로 사이즈 관리 (#268)

--- pennyway-client-iOS/pennyway-client-iOS/Clean/Presentation/Scene/ChatSideMenu/Component/SideMenuCell.swift
+++ pennyway-client-iOS/pennyway-client-iOS/Clean/Presentation/Scene/ChatSideMenu/Component/SideMenuCell.swift
@@ -26,7 +26,7 @@ struct SideMenuCell: View {
 
             if isAlarmCell {
                 Toggle(isOn: $isAlarmOn) {}
-                    .toggleStyle(CustomToggleStyle(hasAppeared: .constant(true)))
+                    .toggleStyle(CustomToggleStyle(hasAppeared: $isAlarmOn, size: .small))
             }
         }
         .frame(maxWidth: .infinity)
