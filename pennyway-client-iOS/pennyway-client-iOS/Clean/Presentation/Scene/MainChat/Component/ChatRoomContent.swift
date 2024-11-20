75c693d4737f69a166582c76677b7176e14c542c
yanni <122153297+yanni13@users.noreply.github.com>
2024-11-20 22:23:30 +0900

    feat: 메인채팅뷰에서 셀을 꾹 눌렀을 경우 나가기버튼 표시되던 오류 해결

--- pennyway-client-iOS/pennyway-client-iOS/Clean/Presentation/Scene/MainChat/Component/ChatRoomContent.swift
+++ pennyway-client-iOS/pennyway-client-iOS/Clean/Presentation/Scene/MainChat/Component/ChatRoomContent.swift
@@ -24,6 +24,8 @@ struct ChatRoomContent: View {
                                     isPopUp = true
                                     selectedChatRoom = chatRoom
                                 })
+                                .buttonStyle(PlainButtonStyle())
+                                .buttonStyle(BasicButtonStyleUtil())
                             }
                             .transition(.move(edge: .trailing).combined(with: .opacity))
                         }
@@ -95,6 +97,8 @@ struct ChatRoomCell: View, ImageLoadable {
                         .background(Color("Red03"))
                         
                     })
+                    .transition(.opacity)
+                    .opacity(isShowingDeleteButton ? 1 : 0) // 버튼이 보이는 경우에만 완전 표시
                 }
             }
             // 채팅방 셀
@@ -186,7 +190,7 @@ struct ChatRoomCell: View, ImageLoadable {
                     .onChanged { value in
                         withAnimation {
                             if value.translation.width < 0, isMyChat { // 내 채팅방일 때만 스와이프 가능
-                                offset = max(value.translation.width, -90)
+                                offset = max(value.translation.width, -110)
                                 isShowingDeleteButton = true
                             } else {
                                 offset = 0
@@ -199,9 +203,11 @@ struct ChatRoomCell: View, ImageLoadable {
                             if value.translation.width < -80, isMyChat {
                                 // 스와이프가 80pt 이상이면 삭제 버튼 표시
                                 offset = -90 * DynamicSizeFactor.factor()
+                                isShowingDeleteButton = true
                             } else {
                                 // 그렇지 않으면 원래 위치로 복구
                                 offset = 0
+                                isShowingDeleteButton = false
                             }
                         }
                     }
