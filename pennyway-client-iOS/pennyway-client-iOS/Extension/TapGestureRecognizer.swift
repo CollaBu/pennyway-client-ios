23f995a6a59625c032d4eb8e872095de70791948
heejinnn <gml0593@gmail.com>
2024-11-21 14:10:56 +0900

    feat: 채팅뷰에서 키보드 제어 코드 구현

--- pennyway-client-iOS/pennyway-client-iOS/Extension/TapGestureRecognizer.swift
+++ pennyway-client-iOS/pennyway-client-iOS/Extension/TapGestureRecognizer.swift
@@ -1,15 +1,28 @@
 import SwiftUI
 
 extension UIApplication {
+    static var shouldDismissKeyboard: Bool = true
+
     func addTapGestureRecognizer() {
         guard let window = windows.first else {
             return
         }
-        let tapGesture = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
+
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture(_:)))
         tapGesture.requiresExclusiveTouchType = false
         tapGesture.cancelsTouchesInView = false
         tapGesture.delegate = self
         window.addGestureRecognizer(tapGesture)
+    }
+
+    @objc private func handleTapGesture(_: UITapGestureRecognizer) {
+        guard let window = windows.first else {
+            return
+        }
+
+        if UIApplication.shouldDismissKeyboard {
+            window.endEditing(true) // 키보드 닫기
+        }
     }
 }
 
