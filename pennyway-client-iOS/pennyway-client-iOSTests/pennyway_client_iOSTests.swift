5985b10e2b2abb7a440fb0fd1ac4d68f0c74b027
heejinnn <gml0593@gmail.com>
2024-12-01 18:48:46 +0900

    feat: secret 키 추가

--- pennyway-client-iOS/pennyway-client-iOSTests/pennyway_client_iOSTests.swift
+++ pennyway-client-iOS/pennyway-client-iOSTests/pennyway_client_iOSTests.swift
@@ -2,23 +2,26 @@
 //  pennyway_client_iOSTests.swift
 //  pennyway-client-iOSTests
 //
-//  Created by 최희진 on 11/12/24.
+//  Created by 최희진 on 12/1/24.
 //
 
 import XCTest
 
 final class pennyway_client_iOSTests: XCTestCase {
-    override func setUpWithError() throws {}
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
 
-    override func tearDownWithError() throws {}
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
 
     func testExample() throws {
-        let data1 = 1
-        let data2 = 1
-
-        let result = data1 + data2
-
-        XCTAssertEqual(result, 2)
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
 
     func testPerformanceExample() throws {
