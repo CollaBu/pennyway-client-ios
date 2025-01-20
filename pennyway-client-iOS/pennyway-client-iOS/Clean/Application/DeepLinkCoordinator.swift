fdd30a2a2dbb9d98692ba35d4ce59becf275b8a7
신얀 <shinyan@shinyan.local>
2025-01-20 22:27:37 +0900

    feat: 딥링크 폴더 구조 정의 및 파일 생성

---
+++ pennyway-client-iOS/pennyway-client-iOS/Clean/Application/DeepLinkCoordinator.swift
@@ -0,0 +1,14 @@
+//
+//  DeepLinkCoordinator.swift
+//  pennyway-client-iOS
+//
+//  Created by 신얀 on 1/20/25.
+//
+
+import Foundation
+
+final class DeepLinkCoordinator {
+    func handle(deepLink: DeepLink) {
+        // 화면 전환 로직
+    }
+}
