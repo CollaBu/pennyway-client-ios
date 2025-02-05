39c859438a03a880b5c7f7902986386f22fa605c
heejinnn <gml0593@gmail.com>
2025-02-05 17:17:47 +0900

    fix: 채팅방 스크롤 오류 수정

--- pennyway-client-iOS/pennyway-client-iOS/Clean/Presentation/Scene/ChatRoom/Component/ChatContent.swift
+++ pennyway-client-iOS/pennyway-client-iOS/Clean/Presentation/Scene/ChatRoom/Component/ChatContent.swift
@@ -120,7 +120,9 @@ struct ChatContent: View {
             }
             .onAppear {
                 // 처음 열릴 때 가장 아래로 스크롤
-                proxy.scrollTo("bottom", anchor: .bottom) // TODO: 공유메시지인 경우에만 아래로 스크롤되지 않는 오류
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                    proxy.scrollTo("bottom", anchor: .bottom)
+                }
             }
             .onTapGesture {
                 UIApplication.shouldDismissKeyboard = true // 빈 화면 터치 시 키보드 닫기
